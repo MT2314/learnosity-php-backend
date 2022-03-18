3635ce2d99607112697534be7521daa057933e20
Emily <eleblanc@tvo.org>
2022-03-18 11:17:57 -0400

    added production url to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: 
+      argv.mode === "development"
+        ? "http://localhost:3001/"
+        : "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/wip/el-demo-component-library/index.html"
   },
 
   resolve: {
@@ -66,4 +69,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
