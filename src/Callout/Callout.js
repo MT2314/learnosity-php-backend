f19561c0bdb10e5be8034352e597435c2a70f411
Sam Beaulieu <sbeaulieu@tvo.org>
2022-03-21 15:55:11 -0400

    Added styled-components to Callout as test for bringing styles over in mod. fed.

--- src/Callout/Callout.js
+++ src/Callout/Callout.js
@@ -2,11 +2,12 @@
 import React, { useEffect } from "react";
 import { Paper } from "@mui/material";
 import NativeSelect from '@mui/material/NativeSelect';
-import styles from "./Callout.module.scss";
+// import styles from "./Callout.module.scss";
 // import { useNode } from "@craftjs/core";
 // import { ComponentControl } from "components/Control";
 // import { useConfigContext } from "context";
 import TextEditable from "../TextEditable";
+import styled from 'styled-components';
 
 const Callout = (props) => {
   //const { config } = useConfigContext();
@@ -15,21 +16,55 @@ const Callout = (props) => {
   // useEffect(() => {
   //   setCustom(custom => Object.assign(custom, {type: "callout"}));
   // }, []);
+
+  const Dropdown = styled(NativeSelect)`
+    margin-bottom: 10px;
+    background: red;
+  `;
+
+  const HeadingText = styled(TextEditable)`
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 43px;
+    font-family: "SuisseIntl";
+  `;
+
+  // What used to be "Callout_body_text"
+  const Container = styled.div` 
+    display: flex;
+  `;
+
+  const CalloutBody = styled(TextEditable)`
+    margin-left: 53px;
+    font-family: "SuisseIntl";
+  `;
+
+  const CallOutImg = styled.img`
+    margin-right: 10px;
+  `;
+
+  const IconPlaceholder = styled.div`
+    border: 1px dashed silver;
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+  `;
+
   return (
     <Paper aria-label="Callout" className="component">  
       <label htmlFor={`callout-type`}>Callout Type</label>&nbsp;
-      <NativeSelect autoFocus id={`callout-type`} value={calloutType||""} onChange={e => setProp(props => Object.assign(props, { calloutType: e.target.value }))} className={styles.Callout_type_dropdown}>
+      <Dropdown autoFocus id={`callout-type`} value={calloutType||""} onChange={e => setProp(props => Object.assign(props, { calloutType: e.target.value }))}>
         <option value={""}>Select Value</option>
-      </NativeSelect>
-      <div className={styles.Callout_body_text} role="presentation">
+      </Dropdown>
+      <Container role="presentation">
         {/* decorative icon */}
         {calloutType ?
-          (<img className={styles.Callout_img} alt={""} aria-label="Callout type icon"/>)
-        : (<div className={styles.Callout_icon_placeholder} aria-label="Callout type icon placeholder"></div>)}
-        <TextEditable style={{minWidth: '220px'}} placeholder="Callout heading text" onChange={e => setProp(props => Object.assign(props, { heading: e.target.value }))} className={styles.Callout_heading} value={heading||""} />
-      </div>
+          (<CallOutImg alt={""} aria-label="Callout type icon"/>)
+        : (<IconPlaceholder aria-label="Callout type icon placeholder"></IconPlaceholder>)}
+        <HeadingText style={{minWidth: '220px'}} placeholder="Callout heading text" onChange={e => setProp(props => Object.assign(props, { heading: e.target.value }))} value={heading||""} />
+      </Container>
         
-        <TextEditable placeholder="Callout body text"  multiline={true} onChange={e => setProp(props => Object.assign(props, { body: e.target.value }))} className={styles.Callout_body} value={body||""} />
+        <CalloutBody placeholder="Callout body text"  multiline={true} onChange={e => setProp(props => Object.assign(props, { body: e.target.value }))} value={body||""} />
         {/* <ComponentControl nodeId={id} /> */}
     </Paper>
   )
