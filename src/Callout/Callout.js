5105ed7ae8ccf533072082b6b6dd212150ee7f6c
Sam Beaulieu <sbeaulieu@tvo.org>
2022-03-23 08:37:31 -0400

    Callout w/ styled-components "done"

--- src/Callout/Callout.js
+++ src/Callout/Callout.js
@@ -17,20 +17,27 @@ const Callout = (props) => {
   //   setCustom(custom => Object.assign(custom, {type: "callout"}));
   // }, []);
 
+  const Body = styled(Paper)`
+    background: #ececec !important;
+    padding: 36px 100px;
+  `
+
+  const CalloutLabel = styled.label`
+    font: 18px / 22.5px Arial, sans-serif;
+  `
+
   const Dropdown = styled(NativeSelect)`
     margin-bottom: 10px;
-    background: red;
-
-    &:hover {
-      border: 5px solid rebeccapurple;
-    }
   `;
 
   const HeadingText = styled(TextEditable)`
     font-size: 20px;
     font-weight: 700;
     line-height: 43px;
-    font-family: "SuisseIntl";
+    font-family: "Arial";
+    min-width: 220px;
+    width: 230px;
+    color: #000 !important;
   `;
 
   // What used to be "Callout_body_text"
@@ -40,37 +47,45 @@ const Callout = (props) => {
 
   const CalloutBody = styled(TextEditable)`
     margin-left: 53px;
-    font-family: "SuisseIntl";
+    height: 15px;
+    background: transparent;
+    border: 1px solid transparent;
+    font-family: "Arial";
+    color: #000;
   `;
 
-  const CallOutImg = styled.img`
+  const CalloutImg = styled.img`
     margin-right: 10px;
   `;
 
   const IconPlaceholder = styled.div`
     border: 1px dashed silver;
-    width: 40px;
-    height: 40px;
+    width: 43px;
+    height: 43px;
     margin-right: 10px;
   `;
 
   return (
-    <Paper aria-label="Callout" className="component">  
-      <label htmlFor={`callout-type`}>Callout Type</label>&nbsp;
+    <Body aria-label="Callout">  
+      <CalloutLabel htmlFor={`callout-type`}>Callout Type</CalloutLabel>&nbsp;
       <Dropdown autoFocus id={`callout-type`} value={calloutType||""} onChange={e => setProp(props => Object.assign(props, { calloutType: e.target.value }))}>
         <option value={""}>Select Value</option>
+        <option value={""}>Challenge</option>
+        <option value={""}>Notebook</option>
+        <option value={""}>Try It</option>
+        <option value={""}>Definition</option>
       </Dropdown>
       <Container role="presentation">
         {/* decorative icon */}
         {calloutType ?
-          (<CallOutImg alt={""} aria-label="Callout type icon"/>)
+          (<CalloutImg alt={""} aria-label="Callout type icon"/>)
         : (<IconPlaceholder aria-label="Callout type icon placeholder"></IconPlaceholder>)}
-        <HeadingText style={{minWidth: '220px'}} placeholder="Callout heading text" onChange={e => setProp(props => Object.assign(props, { heading: e.target.value }))} value={heading||""} />
+        <HeadingText value="Enter Heading" placeholder="Enter Heading" onChange={e => setProp(props => Object.assign(props, { heading: e.target.value }))} />
       </Container>
         
-        <CalloutBody placeholder="Callout body text"  multiline={true} onChange={e => setProp(props => Object.assign(props, { body: e.target.value }))} value={body||""} />
+        <CalloutBody placeholder="Enter Body"  multiline={true} onChange={e => setProp(props => Object.assign(props, { body: e.target.value }))} value="Enter Body" />
         {/* <ComponentControl nodeId={id} /> */}
-    </Paper>
+    </Body>
   )
 }
 
