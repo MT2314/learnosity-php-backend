e4a66e50d5cface51f1e0db8e6a90db8ccea003f
Sam Beaulieu <sbeaulieu@tvo.org>
2022-03-23 09:59:00 -0400

    Added CSS module version of Callout to webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,7 @@ module.exports = (_, argv) => ({
       exposes: {
         "./TextEditable": "./src/TextEditable/TextEditable.js",
         "./QuoteBox":"./src/QuoteBox/QuoteBox.js",
+        "./Callout":"./src/CalloutSC/Callout.js",
         "./CalloutSC":"./src/CalloutSC/CalloutSC.js",
         "./Header":"./src/Header.jsx"
       },
