8bffb27a35abaaae253f82a48622aabdfd5bb547
Sam Beaulieu <sbeaulieu@tvo.org>
2022-04-13 16:31:12 -0400

    Reformatted image canvas div

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -33,31 +33,29 @@ const Image = () => {
          className={styles.Image__container}
          elevation={0}
       >
-         { context.uploadedImg ? (
+			<div 
+            // className={
+				// 	context.uploadedImg ? `${styles.Image__canvasImgContainer} ${styles.Image__holdingbox}`
+				// 	: styles.Image__holdingbox
+				// }
+				className={styles.Image__canvasImgContainer}
+         >
+         {context.uploadedImg ? (
             // If image has been uploaded
             <>
             {/* add link to image */}
-            {
-               context.imgLink ?
+            {context.imgLink ?
                <a href={context.imgLink}>
-                  <div 
-                     className={styles.Image__canvasImgContainer}
-                     tabIndex="0"
-                  >
-                     <img
-                        src={context.uploadedImg}
-                        alt={context.alt}
-                        className={styles.Image__img}
-                        style={imgSizeStyles}
-                     />
-                  </div>
+                  <img
+                     src={context.uploadedImg}
+                     alt={context.alt}
+                     className={styles.Image__img}
+                     style={imgSizeStyles}
+                  />
                </a>
                :
                // no link added to img
-               <div 
-               className={styles.Image__canvasImgContainer}
-               tabIndex="0"
-            >
+               <>
                <img
                   src={context.uploadedImg}
                   alt={context.alt}
@@ -65,33 +63,33 @@ const Image = () => {
                   tabIndex="0"
                   style={imgSizeStyles}
                />
-            </div>
+            	</>
             }
             </>
             ) : (
             // If no image has been uploaded
-            <div
-               className={styles.Image__holdingbox}
-            >
+            <>
                <div className={styles.Image__outline} tabIndex="0"></div>
-               <textarea
-                  placeholder="Type caption here..."
-                  aria-label="Add caption text to image"
-                  rows={2}
-                  className={styles.Image__caption}
-               ></textarea>
-            </div>
+            </>
             )
          }
-         <label hmtlFor='credit' className={styles.srOnly}>Enter credit for image</label>
-         <input type="text"
-                name="credit"
-                id="credit"
-                value={credit}
-                placeholder="add image credit"
-                className={styles.Image__caption}
-                onChange={e => setCredit(e.target.value)}
-         />
+				<textarea
+         	   placeholder="Type caption here..."
+         	   aria-label="Add caption text to image"
+         	   rows={2}
+         	   className={styles.Image__caption}
+         	></textarea>
+         	<label hmtlFor='credit' className={styles.srOnly}>Enter credit for image</label>
+         	<input
+					type="text"
+         	   name="credit"
+         	   id="credit"
+         	   value={credit}
+         	   placeholder="add image credit"
+         	   className={styles.Image__caption}
+         	   onChange={e => setCredit(e.target.value)}
+         	/>
+			</div>
       </Paper>
    );
 };
