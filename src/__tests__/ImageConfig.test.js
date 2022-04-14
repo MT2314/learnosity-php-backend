9ccd9e0762142f97a430be16d82fe3fc7cab0703
Emily <eleblanc@tvo.org>
2022-04-14 10:28:14 -0400

    config unit test

--- src/__tests__/ImageConfig.test.js
+++ src/__tests__/ImageConfig.test.js
@@ -5,14 +5,25 @@ import { ImageProvider } from "../Image/ImageProvider";
 
 describe("<ImageConfig />", () => {
 
+   const testComponent =   
+   <>
+   <ImageProvider>
+      <ImageConfig />
+   </ImageProvider>
+   </>
+
    it('renders ImageConfig component', () => {
-      expect(render(
-         <>
-         <ImageProvider>
-            <ImageConfig />
-         </ImageProvider>
-         </>
-      )).toBeDefined()
+      expect(render(testComponent)).toBeDefined()
    })
+
+   it('renders label, input and submit btn for img link', () => {
+      render(testComponent)
+
+      expect(screen.getByLabelText("Add link to image")).toBeDefined();
+      expect(screen.getByPlaceholderText("https://example.com")).toBeDefined();
+      expect(screen.getByText("Add Link")).toBeDefined();
+   })
+
+
    
 })
