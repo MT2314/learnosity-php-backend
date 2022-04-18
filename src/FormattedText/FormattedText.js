02232df760258cb9e69b136f57f35dee80e7b34e
Sahil <spatel@tvo.org>
2022-04-18 16:23:29 -0400

    Created separate file for toolbar options

--- src/FormattedText/FormattedText.js
+++ src/FormattedText/FormattedText.js
@@ -2,55 +2,30 @@ import React, { useState } from "react";
 import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "./FormattedText.style.css"
 
-const FormattedText = ({placeHolderText}) => {
+import toolbarOptions from "./Utility/toolbarOptions.js";
+import "./FormattedText.style.css";
 
-   const [editorState, setEditorState] = useState(() => EditorState.createEmpty()
-   );
-
-   const handleEditorChange = (state) => {
-      setEditorState(state);
-   };
-
-   const toolBarObject = {
-      options: ["inline", "textAlign", "list", "link"],
-      inline: {
-         options: [
-            "bold",
-            "italic",
-            "underline",
-            "strikethrough",
-            "superscript",
-            "subscript",
-         ],
-         bold: { className: "bordered-option-classname" },
-         italic: { className: "bordered-option-classname" },
-         underline: { className: "bordered-option-classname" },
-         strikethrough: { className: "bordered-option-classname" },
-         code: { className: "bordered-option-classname" },
-      },
-      textAlign: {
-         options: ["left", 'center', 'right', "justify"],
-       },
-       link: {
-         options: ['link', "unlink"],
-       }
-   };
-   return (
-      <div className="App">
-         <Editor
-            editorState={editorState}
-            spellCheck="true"
-            onEditorStateChange={handleEditorChange}
-            wrapperClassName="wrapper-class"
-            editorClassName="editor-class"
-            toolbarClassName="toolbar-class"
-            toolbar={toolBarObject}
-            placeholder={placeHolderText}
-            />
-      </div>
-   );
-}
+const FormattedText = ({ placeHolderText }) => {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  return (
+    <div className="App">
+      <Editor
+        editorState={editorState}
+        spellCheck="true"
+        onEditorStateChange={(state) => {
+          setEditorState(state);
+        }}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        toolbar={toolbarOptions}
+        placeholder={placeHolderText}
+      />
+    </div>
+  );
+};
 
 export default FormattedText;
