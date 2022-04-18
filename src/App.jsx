615281c7a6d7c09d5dd092c1f5042bda83f145a2
Sahil <spatel@tvo.org>
2022-04-18 17:00:15 -0400

    Changed the version number

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Callout from "./Callout/Callout.js";
-import Header from "./Header"
-import Image from './Image/Image';
+import Header from "./Header";
+import Image from "./Image/Image";
 import ImageConfig from "./Image/ImageConfig";
 import FormattedText from "./FormattedText";
 import { ImageProvider } from "./Image/ImageProvider";
 
 import "./index.css";
 
-const App = () => (
-  <>
-  <Header 
-    title="component-library"
-    backgroundColor="salmon"
-  />
-  <div className="container" style={{display:"flex"}}>
-    <div className="canvas" style={{border: "2px solid black"}}>
-      <Callout />
-      <Image /> 
-      <FormattedText />
-    </div>
-    <div className="editPanel" style={{border:"2px solid black", marginLeft: "1em", padding: "10px" }}>
-      <ImageConfig /> 
-    </div>
-  </div>
-  </>
-);
+const App = () => {
+  console.log("3.0.1")
+  return (
+    <>
+      <Header title="component-library" backgroundColor="salmon" />
+      <div className="container" style={{ display: "flex" }}>
+        <div className="canvas" style={{ border: "2px solid black" }}>
+          <Callout />
+          <Image />
+          <FormattedText />
+        </div>
+        <div
+          className="editPanel"
+          style={{
+            border: "2px solid black",
+            marginLeft: "1em",
+            padding: "10px",
+          }}
+        >
+          <ImageConfig />
+        </div>
+      </div>
+    </>
+  );
+};
 ReactDOM.render(
   <ImageProvider>
     <App />
