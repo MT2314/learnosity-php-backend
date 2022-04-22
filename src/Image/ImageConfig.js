3713fbeac1390cb5126935f081a1d2e770634944
Emily <eleblanc@tvo.org>
2022-04-22 15:41:10 -0400

    seperate imageuploader component

--- src/Image/ImageConfig.js
+++ src/Image/ImageConfig.js
@@ -1,53 +1,15 @@
 import React, { useState, useContext } from 'react';
-import { useDropzone } from 'react-dropzone'; 
 import styles from './styles/ImageConfig.module.scss';
 import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
 import NativeSelect from '@mui/material/NativeSelect';
 import { ImageWidgetContext } from './ImageProvider';
+import ImageUploader from './ImageUploader';
 
 function ImageConfig() {
 
   const context = useContext(ImageWidgetContext);
 
   const [ imgLink, setImgLink ] = useState("");
-  const [ count, setCount ] = useState(0);
-  const [file, setFile] = useState([]);
-
-   // image preview
-   const thumbs =  (
-      <div className={styles.ImageConfig__thumbOuter}>
-         <div className={styles.ImageConfig__thumbInner}>
-            <img
-               src={file.preview}
-               alt={context.alt}
-               className={styles.ImageConfig__thumbnailImg}
-            />
-         </div>
-      </div>
-   );
-
-   const imageValidator = (image) => {
-      if (image.size > 5000000) {
-         alert('Image size is greater than 5MB.  Please try uploading a different image.');
-      } else if (image.type !== 'image/jpeg' && image.type !== 'image/gif' && image.type !== 'image/png' && image.type !== 'image/svg+xml') {
-         alert('Image type not accepted.  Please try uploading a different type of image.');
-      }
-   };
-
-//  drop zone image uploader configuration
-  const { getRootProps, getInputProps } = useDropzone({
-      accept: 'image/jpg, image/jpeg, image/gif, image/png, image/svg+xml',
-      maxFiles: 1,
-      multiple: false,
-      maxSize: 5000000,
-      validator: imageValidator,
-      onDrop: acceptedFiles => {
-         handleClearImageFields()
-         context.updateContext({uploadedImg: URL.createObjectURL(acceptedFiles[0])});
-         setFile({preview: URL.createObjectURL(acceptedFiles[0])});
-         URL.revokeObjectURL(file.preview);
-      }
-   });
 
    const altTextCounter = context.alt.length + "/200";
    
@@ -56,21 +18,6 @@ function ImageConfig() {
       context.updateContext({ imgLink: imgLink });
    }
 
-   const handleClearImageFields = () => {
-      setCount(count + 1);
-      setImgLink('');
-      if (count > 0) {
-         context.updateContext({ 
-            alt: "",
-            longDesc: "",
-            imgLink: "",
-            imgSize: "default"
-         });
-      } else {
-         return
-      };
-   };
-
    return (
       <section className={styles.ImageConfig__editPanelContainer}>
          {/* Edit Panel Component Title */}
@@ -78,20 +25,9 @@ function ImageConfig() {
             <InsertPhotoOutlinedIcon className={styles.ImageConfig__componentTitleIcon} />
             <p className={styles.ImageConfig__componentTitle}>Image</p>
          </div>
-         <div className={styles.ImageConfig__section}>
-            {/* Image thumbnail */}
-            <aside className={styles.ImageConfig__thumbnailContainer}>
-               { file.length !== 0 && thumbs }
-            </aside>
-            {/* Image Uploader */}
-            <div {...getRootProps()} className={styles.ImageConfig__uploader}>
-               <label for="image-uploader">Drag 'n' drop some files here, or click to open the file dialog</label>
-               <input name="image-uploader" {...getInputProps()} />
-            </div>
-            <p className={styles.ImageConfig__uploadSize}>
-               Max file size: 5mb, accepted: .jpg, .gif, .png, .svg
-            </p>
-         </div>
+
+         <ImageUploader />
+
          <form onSubmit={handleSubmit} className={styles.ImageConfig__validationForm}>
             <div className={styles.ImageConfig__section}>
                <h2 className={styles.ImageConfig__imageH2}>Alt Text</h2>
