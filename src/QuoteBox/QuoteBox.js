f3155387562cd6d9fecfd4032ecb9fd0b220e3c5
Sam Beaulieu <sbeaulieu@tvo.org>
2022-04-27 14:49:39 -0400

    Added SVG/body/citation to QuoteBox, minor styling

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -4,13 +4,25 @@ import { Paper } from "@mui/material";
 import TextEditable from "../TextEditable/TextEditable";
 import FormattedText from '../FormattedText/FormattedText';
 
-// import styles from './QuoteBox.module.scss';
+import styles from './styles/QuoteBox.module.scss';
 
 const QuoteBox = ({ uuid, uuidClean, type, body, citation, url }) => {
    return (
-      <>
-         <h1>Hello, World!</h1>
-      </>
+      <Paper aria-label="Quote Box" className={styles.quoteBoxContainer}>
+         <div className={styles.quoteBodyContainer}>
+            <div className={styles.quoteIcon}></div>
+            <FormattedText
+               placeHolderText='Type quote body here...'
+               required
+            />
+         </div>
+         <TextEditable
+            className={styles.quoteCitation}
+            placeholder='Type citation here...'
+            value={citation}
+            multiline={false}
+         />
+      </Paper>
    );
 };
 
