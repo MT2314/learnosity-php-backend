a65ad8fe3dc7c9029468a5ad76e960c58020a062
Emily <eleblanc@tvo.org>
2022-04-28 15:52:33 -0400

    changed image provider to default export

--- src/__tests__/Image.test.js
+++ src/__tests__/Image.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Image from '../Image/Image';
-import { ImageProvider } from "../Image/ImageProvider";
+import ImageProvider from "../Image/ImageProvider";
 
 
 describe("<Image />", () => {
