97700de2775163b344d44d7ed84f6e1e4a367131
Karen Bridson <kbridson@tvo.org>
2022-04-29 18:52:12 -0400

    got the titles rendering on toggle now too

--- src/components/Callout/Callout.js
+++ src/components/Callout/Callout.js
@@ -1,39 +1,16 @@
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useState } from "react";
 import { Paper } from "@mui/material";
 import NativeSelect from "@mui/material/NativeSelect";
 import styles from "./Callout.module.scss";
-import TextEditable from "../TextEditable";
 import FormattedText from "../FormattedText/FormattedText";
 import { calloutConfig } from "./utility/calloutConfig";
-import callout from "./calloutOptions";
+import calloutOptions from "./calloutOptions";
 import sampleData from "./sampleDataConfig";
 
-const Callout = ({ body, heading, calloutType }) => {
+const Callout = ({ calloutBody, calloutType }) => {
   const [calloutTypeSvg, setCalloutTypeSvg] = useState("");
-  const [calloutBody, setCalloutBody] = useState("");
-
-  // useEffect((sampleData) => {
-  //   setCalloutBody(body);
-  // }, []);
-
-  // const { heading, calloutType } = props;
-
-  const [state, dispatch] = useReducer(calloutReducer, {
-    body: "",
-    heading: "",
-  });
-  const calloutReducer = (state, { type, payload }) => {
-    switch (type) {
-      case "body":
-        return { ...state, body: payload };
-      case "heading":
-        return { ...state, heading: payload };
-      case "url":
-        return { ...state, url: payload };
-      default:
-        return state;
-    }
-  };
+  const [calloutTitle, setCalloutTitle] = useState("");
+  // const [calloutBody, setCalloutBody] = useState("");
 
   return (
     <Paper aria-label="Callout" className={styles.Callout_main}>
@@ -49,11 +26,14 @@ const Callout = ({ body, heading, calloutType }) => {
         autoFocus
         id={`callout-type`}
         value={calloutType || ""}
-        onChange={(e) => setCalloutTypeSvg(e.target.value)}
+        onChange={(e) => {
+          setCalloutTypeSvg(calloutOptions[e.target.value].iconUrl);
+          setCalloutTitle(calloutOptions[e.target.value].title);
+        }}
         className={styles.Callout_type_dropdown}
       >
-        {callout.map(({ id, title, iconUrl }) => (
-          <option key={id} value={iconUrl}>
+        {calloutOptions.map(({ id, title }) => (
+          <option key={id} value={calloutOptions[id].id}>
             {title}
           </option>
         ))}
@@ -61,40 +41,37 @@ const Callout = ({ body, heading, calloutType }) => {
       <div className={styles.Callout_body_text} role="presentation">
         {/* decorative icon */}
         {calloutTypeSvg ? (
-          <img
-            className={styles.Callout_img}
-            src={calloutTypeSvg}
-            alt={""}
-            aria-label="Callout type icon"
-          />
+          <>
+            <img
+              className={styles.Callout_img}
+              src={calloutTypeSvg}
+              alt={""}
+              aria-label="Callout type icon"
+            />
+            <p
+              placeholder="Callout heading text will appear here"
+              className={styles.Callout_heading}
+            >
+              {calloutTitle}
+            </p>
+          </>
         ) : (
           <div
             className={styles.Callout_icon_placeholder}
             aria-label="Callout type icon placeholder"
           ></div>
         )}
-        {console.log("here is svgtype ", calloutTypeSvg)}
-        <TextEditable
-          placeholder="Callout heading text"
-          value={sampleData[0].heading}
-          onChange={(e) =>
-            dispatch({ type: "heading", payload: e.target.value })
-          }
-          className={styles.Callout_heading}
-        />
+        <p className={styles.Callout_heading}></p>
       </div>
-      {console.log("here is heading ", sampleData[0].heading)}
       <FormattedText
-        // placeHolderText="Enter callout body text here..."
-        placeHolderText={sampleData[1].body}
+        placeHolderText="Enter callout body text here..."
         toolbar={calloutConfig}
         value={sampleData[0].body}
         className={styles.Callout_body}
         editorClassName="callout_editor_class"
         // onChange={(e) => dispatch({ type: "body", payload: e.target.value })}
-        onChange={(e) => setCalloutBody(e.target.value)}
+        // onChange={(e) => setCalloutBody(e.target.value)}
       />
-      {console.log("here is body ", sampleData[0].body)}
     </Paper>
   );
 };
