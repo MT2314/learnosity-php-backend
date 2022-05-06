4619059341ea8a9278b05a35260305826d368308
Karen Bridson <kbridson@tvo.org>
2022-05-06 08:44:58 -0400

    added more keyboard shortcuts

--- src/components/FormattedText/AccessibilityKeysPage.js
+++ src/components/FormattedText/AccessibilityKeysPage.js
@@ -7,31 +7,36 @@ const AccessibilityKeyPage = () => {
       <p style={{ color: "black", fontSize: "22pt" }}>
         Text Formatting Keyboard Shortcuts
       </p>
-      <table>
-        <thead></thead>
-        <tr>
+      <table style={{ width: "35%" }}>
+        <thead style={{ fontWeight: "bold" }}>
           <td>Style</td>
-          <td>Shortcut</td>
+          <td>Windows Shortcut</td>
+          <td>Mac Shortcut</td>
+        </thead>
+        <tr>
+          <td>Make selected text bold</td>
+          <td>Ctrl + B</td>
+          <td>Cmd + B</td>
         </tr>
         <tr>
-          <td>To make text bold</td>
-          <td>cmd + b</td>
+          <td>Make selected text italic</td>
+          <td>Ctrl + I</td>
+          <td>Cmd + I</td>
         </tr>
         <tr>
-          <td>To make text italic</td>
-          <td>cmd + i</td>
+          <td>Make selected text underlined</td>
+          <td>Ctrl + U</td>
+          <td>Cmd + U</td>
         </tr>
         <tr>
-          <td>To underline text</td>
-          <td>cmd + u</td>
+          <td>Make selected text superscript</td>
+          <td>Ctrl + Shift + =</td>
+          <td>Cmd + Shift + =</td>
         </tr>
         <tr>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
+          <td>Make selected text subscript</td>
+          <td>Ctrl + =</td>
+          <td>Cmd + =</td>
         </tr>
       </table>
     </>
