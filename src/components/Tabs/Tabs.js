4ad3d788d2f87895be5dede010c562fbf0040aa1
Emily <eleblanc@tvo.org>
2022-05-11 15:47:54 -0400

    mapping through tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,36 +1,64 @@
-import React from 'react';
+import React from "react";
 
-import FormattedText from '../FormattedText';
-
+import FormattedText from "../FormattedText";
 
 export const defaultProps = {
-   tabsIntroduction: null,
-   tabs: null,
+  tabsIntroduction: null,
+  tabs: [],
+};
 
- };
+const TabContainer = ({ children }) => {
+  return <div>{children}</div>;
+};
 
-
- const Tabs = (
-    { 
-      type, 
-      tabsIntroduction, 
-      tabs, 
-      setProp = () => {}}) => {
-         
-         const mockData = {
-            type: "tabs",
-            tabs: [
-               {
-                  tabLabel: "",
-                  components: [
-                  ]
-               },
-               {}
-            ]
-         }
-       
-         return (<div>hello world!</div>);
+const TabTitle = () => {
+   return <h3> TAB TabTitle </h3>
 }
 
+const Tabs = ({
+  type = "tabs",
+  tabsIntroduction = null,
+  tabs,
+  setProp = () => {},
+}) => {
+
+   tabs = [
+   {
+      tabLabel: "Geography",
+      components: [<FormattedText/>]
+   },
+   {
+      tabLabel: "Science",
+      components: [<FormattedText/>,<FormattedText/>]
+   },
+   {
+      tabLabel: "Math",
+      components: [<FormattedText/>,<FormattedText/>]
+   }]
+   console.log(tabs, type)
+  return (
+    <TabContainer>
+      <p> I am the tab container</p>
+      {
+         tabs.map((tab) => {
+            return (
+               <>
+               <p>{tab.tabLabel}</p>
+               <div>
+                  {
+                     tab.components.map((component) => {
+                        return(
+                           <div>{component}</div>
+                        )
+                     })
+                  }
+               </div>
+               </>
+            )
+         })
+      }
+    </TabContainer>
+  );
+};
 
 export default Tabs;
