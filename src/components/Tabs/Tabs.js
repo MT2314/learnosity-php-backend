de705055d87a56087d6a86364f3c6dfa8a0bfb1c
Sam Beaulieu <sbeaulieu@tvo.org>
2022-05-11 10:59:55 -0400

    mocked data structure for tabs

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Tabs = ({ uuid, uuidClean, type, introduction, tabs }) => {
+
+   const mockData = {
+      uuid: "",
+      uuidClean: "",
+      type: "tabs",
+      tabs: [
+         {
+            uuid: "",
+            type: "formattedText",
+            tabLabel: "",
+            components: [
+               
+            ]
+         },
+         {}
+      ]
+   }
+
+   return (
+      <h1>Hello, World I am a Tabs!</h1>
+   )
+}
