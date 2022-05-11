607ce3c9a5ea4f31869b434ea897349596867e00
Emily <eleblanc@tvo.org>
2022-05-11 12:27:05 -0400

    mui tabs added

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormattedText from '../FormattedText';
+import { Tab, Box } from '@mui/material';
+import { TabPanel, TabContext, TabList } from "@mui/lab";
 
 
 export const defaultProps = {
@@ -9,34 +11,53 @@ export const defaultProps = {
  };
 
 
-const Tabs = (
-   { uuid, 
+ const TabWidget = (
+    { uuid, 
       uuidClean, 
       type, 
       introduction, 
       tabs, 
       setProp = () => {}}) => {
-
-   const mockData = {
-      uuid: "",
-      uuidClean: "",
-      type: "tabs",
-      tabs: [
-         {
+         
+         const mockData = {
             uuid: "",
-            type: "formattedText",
-            tabLabel: "",
-            components: [
-               
+            uuidClean: "",
+            type: "tabs",
+            tabs: [
+               {
+                  uuid: "",
+                  type: "formattedText",
+                  tabLabel: "",
+                  components: [
+                     
+                  ]
+               },
+               {}
             ]
-         },
-         {}
-      ]
-   }
+         }
+         const [value, setValue] = useState('1');
 
-   return (
-      <h1>Hello, World I am a Tabs!</h1>
-   )
+         const handleChange = (event, newValue) => {
+           setValue(newValue);
+         };
+       
+         return (
+           <Box sx={{ width: '100%', typography: 'body1' }}>
+             <TabContext value={value}>
+               <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                 <TabList onChange={handleChange} aria-label="lab API tabs example">
+                   <Tab label="Item One" value="1" />
+                   <Tab label="Item Two" value="2" />
+                   <Tab label="Item Three" value="3" />
+                 </TabList>
+               </Box>
+               <TabPanel value="1">Item One</TabPanel>
+               <TabPanel value="2">Item Two</TabPanel>
+               <TabPanel value="3">Item Three</TabPanel>
+             </TabContext>
+           </Box>
+         );
 }
 
-export default Tabs;
+
+export default TabWidget;
