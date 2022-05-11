e638fa10ad0a2d49ee3f5cab135abd3a51ccd6bf
Emily <eleblanc@tvo.org>
2022-05-11 12:30:07 -0400

    added lorem

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -46,14 +46,14 @@ export const defaultProps = {
              <TabContext value={value}>
                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                  <TabList onChange={handleChange} aria-label="lab API tabs example">
-                   <Tab label="Item One" value="1" />
-                   <Tab label="Item Two" value="2" />
-                   <Tab label="Item Three" value="3" />
+                   <Tab label="Kangaroos" value="1" />
+                   <Tab label="Cats" value="2" />
+                   <Tab label="Dogs" value="3" />
                  </TabList>
                </Box>
-               <TabPanel value="1">Item One</TabPanel>
-               <TabPanel value="2">Item Two</TabPanel>
-               <TabPanel value="3">Item Three</TabPanel>
+               <TabPanel value="1">The kangaroo is a marsupial from the family Macropodidae. In common use the term is used to describe the largest species from this family, the red kangaroo, as well as the antilopine kangaroo, eastern grey kangaroo, and western grey kangaroo. </TabPanel>
+               <TabPanel value="2">The cat is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.</TabPanel>
+               <TabPanel value="3">The dog or domestic dog is a domesticated descendant of the wolf, and is characterized by an upturning tail. The dog is derived from an ancient, extinct wolf, and the modern wolf is the dog's nearest living relative.</TabPanel>
              </TabContext>
            </Box>
          );
