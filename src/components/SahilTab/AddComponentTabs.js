8428799531e5126c17e8585c9c9dd424f92a3d8d
Sahil <spatel@tvo.org>
2022-05-13 15:58:13 -0400

    Addded dynamic selection for the component

--- src/components/SahilTab/AddComponentTabs.js
+++ src/components/SahilTab/AddComponentTabs.js
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
+import { InputLabel, MenuItem, FormControl, Select } from "@mui/material";
+import FormattedText from "../FormattedText";
 
 const AddComponentTabs = () => {
-    const[dropDownValue, setDropDownValue] = useState("")
+  const [dropDownValue, setDropDownValue] = useState("");
+  const handleChange = (event) => {
+    setDropDownValue(event.target.value);
+  };
   return (
     <>
-      <div>Adding Component here</div>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">
+          Select Component
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={dropDownValue}
+          onChange={handleChange}
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value="formattedText">FormattedText</MenuItem>
+          <MenuItem value="image">Image</MenuItem>
+          <MenuItem value="video">Video</MenuItem>
+        </Select>
+      </FormControl>
+
+      {dropDownValue === "formattedText" && <FormattedText />}
     </>
   );
 };
