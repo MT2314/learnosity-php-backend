02590d225167a29b137d7be7f4e0975062b20f30
Sahil <spatel@tvo.org>
2022-05-26 15:03:38 -0400

    Changed version from 17.0.1 to 17.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import ImagePage from "./pages/ImagePage";
 import "./index.css";
 
 const App = () => {
-  console.log("17.0.1");
+  console.log("17.0.2");
   return (
     <>
       <WidgetContextProvider>
