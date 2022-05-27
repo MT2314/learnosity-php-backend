297bedd087948181cf8b0d1f5cee3e8b54a4f063
Sahil <spatel@tvo.org>
2022-05-27 14:49:07 -0400

    Added ImageConfig Test Case

---
+++ src/__tests__/ImageConfig.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ImageConfig from "../Image/ImageConfig";
+import ImageProvider from "../Image/ImageProvider";
+
+describe("<ImageConfig />", () => {
+
+   const testComponent =   
+   <>
+   <ImageProvider>
+      <ImageConfig />
+   </ImageProvider>
+   </>
+
+   it('renders ImageConfig component', () => {
+      expect(render(testComponent)).toBeDefined();
+   })
+
+   it('Image link renders label, input and submit btn', () => {
+      render(testComponent);
+
+      expect(screen.getByLabelText('Add link to image')).toBeDefined();
+      expect(screen.getByPlaceholderText("https://example.com")).toBeDefined();
+      expect(screen.getByText("Add Link")).toBeDefined();
+   })
+
+   it('Image size render drop down options', () => {
+      render(testComponent);
+
+      expect(screen.getByText("Default")).toBeDefined();
+      expect(screen.getByText("Small")).toBeDefined();
+      expect(screen.getByText("Medium")).toBeDefined();
+      expect(screen.getByText("Large")).toBeDefined();
+   })
+
+   it('Upload button renders', () => {
+      render(testComponent);
+
+      expect(screen.getByText("Upload")).toBeDefined();
+      
+   })
+   
+})
