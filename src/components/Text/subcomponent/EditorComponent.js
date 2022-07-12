9cb0b8e55c4848246812566becb1005ff5cbce7d
Emily <eleblanc@tvo.org>
2022-07-12 11:56:26 -0400

    added textRef

--- src/components/Text/subcomponent/EditorComponent.js
+++ src/components/Text/subcomponent/EditorComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import CustomToolBar from "./CustomToolBar";
@@ -7,7 +7,40 @@ import { v4 as uuidv4 } from "uuid";
 import "quill-paste-smart";
 
 const EditorComponent = ({ body, setProp }) => {
+
   const toolbarId = `unique-id-${uuidv4()}`;
+
+  const [ editorIsFocus, setEditorIsFocus ] = useState(true)
+
+  function useOnClickOutside(ref, handler) {
+    useEffect(
+      () => {
+        const listener = (event) => {
+          // Do nothing if clicking ref's element or descendent elements
+          if (!ref.current || ref.current.contains(event.target)) {
+            return;
+          }
+  
+          handler(event);
+        };
+  
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+  
+        return () => {
+          document.removeEventListener("mousedown", listener);
+          document.removeEventListener("touchstart", listener);
+        };
+      },
+      // Add ref and handler to effect dependencies
+      // It's worth noting that because passed in handler is a new ...
+      // ... function on every render that will cause this effect ...
+      // ... callback/cleanup to run every render. It's not a big deal ...
+      // ... but to optimize you can wrap handler in useCallback before ...
+      // ... passing it into this hook.
+      [ref, handler]
+    );
+  }
 
   const formats = [
     "bold",
@@ -57,10 +90,23 @@ const EditorComponent = ({ body, setProp }) => {
   };
 
   const focusRef = useRef(null);
+  const textRef = useRef(null);
 
   useEffect(() => {
     focusRef.current.focus();
+    console.log(textRef)
   }, []);
+
+  useEffect(() => {
+    window.onclick = (event) => {
+      if (event.target.contains(textRef.current)
+        && event.target !== textRef.current) {     
+        console.log(`You clicked Outside the box!`);
+      } else {     
+        console.log(`You clicked Inside the box!`);
+      }
+    }
+}, []);
 
   const handleDataChange = (content, delta, source, editor) => {
     let editorContent = editor.getContents();
@@ -68,8 +114,8 @@ const EditorComponent = ({ body, setProp }) => {
   };
 
   return (
-    <div className="text-editor" data-testid="text-editor-component">
-      <div className="showtool">
+    <div ref={textRef} className="text-editor" data-testid="text-editor-component">
+      <div className="showtool" style={editorIsFocus ? { display: 'flex'} : { display: 'none'}}>
         <CustomToolBar toolbarId={toolbarId} />
       </div>
       <ReactQuill
