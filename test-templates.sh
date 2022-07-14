9688ab072257280ded898b2615402aeac60955f5
Karen Bridson <kbridson@tvo.org>
2022-07-14 13:21:59 -0400

    got it working again & found problem

--- test-templates.sh
+++ test-templates.sh
@@ -2,6 +2,9 @@
 # copy component-library templates to ilc-amp-2 node modules... This emulates a "build" of the component library
 cp -R -a ./src/templates ./amp-template-generator/node_modules/@publishing-platform/component-library/dist
 
+# cp -R -a ./src/templates/test ./amp-template-generator/node_modules/@publishing-platform/component-library/dist
+
+# cp -R -a ./src/templates/views ./amp-template-generator/views
 # "install" the copied files into the right place in ilc-amp-2
 npm run install-templates --prefix ./amp-template-generator
 
