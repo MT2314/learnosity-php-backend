220a2962b7206737bce2921ea70913e35fd233c9
Sam Beaulieu <sbeaulieu@tvo.org>
2022-07-21 14:38:04 -0400

    Added back serialization

--- src/components/Text/subcomponent/EditorComponent.js
+++ src/components/Text/subcomponent/EditorComponent.js
@@ -113,6 +113,7 @@ const EditorComponent = ({ body, setProp }) => {
           nisi ut aliquip ex ea commodo consequat."
         className="quillEditor"
         onChange={handleDataChange}
+        value={body}
       />
     </div>
   );
