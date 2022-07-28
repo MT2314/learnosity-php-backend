4925a78b11d26c09c67f7b3344c533e78a8634a2
Emily <eleblanc@tvo.org>
2022-07-28 17:28:51 -0400

    tab styled

--- src/components/TabPrep/TabPrep.js
+++ src/components/TabPrep/TabPrep.js
@@ -1,9 +1,9 @@
 import React, { createContext, useState, useReducer } from "react";
 //import components
 import TabEl from "./subcomponents/TabEl";
-import "./styles/Tab.scss"
+import "./styles/Tab.scss";
 //import immer
-import produce from 'immer';
+import produce from "immer";
 
 export const defaultProps = {
   layout: [
@@ -25,58 +25,68 @@ export const defaultProps = {
 //state of Layout
 export const LayoutContext = createContext();
 //layout provider wraps the tab component
-export const LayoutProvider = ({children}) => {
-  const { layout } = defaultProps
-  
-  const [ state, dispatch ] = useReducer( 
-    produce(
-        (draft, action) => {
-         switch(action.func){
-           case 'ADD_TAB': 
-            draft.push({
-              id: action.id, 
-              title: action.title,
-              components: []
-            })
-               break
-            case 'REMOVE_TAB':
-              draft.splice( action.tabIndex, 1)
-            break
-            case 'ADD_COMPONENT':
-              console.log("action", action)
-              //console.log("here is the dropped item:", action.component)
-              draft[action.tabIndex].components.push({
-                ...action.component
-                })
-              break
-            case 'DELETE_COMPONENT':
-              draft[action.tabIndex].components.splice(action.componentIndex, 1)
-              break
-             default: 
-             break
-         }
-       }
-      
-      )
-    , layout)
+export const LayoutProvider = ({ children }) => {
+  const { layout } = defaultProps;
 
-    return (
-      <LayoutContext.Provider value={[state, dispatch]}>
-        {children}
-      </LayoutContext.Provider>
-    );
-} 
+  const [state, dispatch] = useReducer(
+    produce((draft, action) => {
+      switch (action.func) {
+        case "ADD_TAB":
+          draft.push({
+            id: action.id,
+            title: action.title,
+            components: [],
+          });
+          break;
+        case "REMOVE_TAB":
+          draft.splice(action.tabIndex, 1);
+          break;
+        case "ADD_COMPONENT":
+          console.log("action", action);
+          //console.log("here is the dropped item:", action.component)
+          draft[action.tabIndex].components.push({
+            ...action.component,
+          });
+          break;
+        case "DELETE_COMPONENT":
+          draft[action.tabIndex].components.splice(action.componentIndex, 1);
+          break;
+        default:
+          break;
+      }
+    }),
+    layout
+  );
+
+  return (
+    <LayoutContext.Provider value={[state, dispatch]}>
+      {children}
+    </LayoutContext.Provider>
+  );
+};
 //state for the active tab index
 export const TabContext = createContext();
 
 const TabPrep = ({ layout, setProp = () => {} }) => {
-
   const [activeTab, setActiveTab] = useState(0);
+
+  const _tabs = layout.map((tab, tabIndex) => {
+    return <TabEl tabIndex={tabIndex} tab={tab} />;
+  });
 
   return (
     <LayoutProvider>
       <TabContext.Provider value={[activeTab, setActiveTab]}>
         <div className="tab-container">
+          <div className="tab-titles">
+            {layout.map((tab, tabIndex) => {
+              return (
+                <div className="tab-title">
+                  <h3 onClick={() => setActiveTab(tabIndex)}>{tab.title}</h3>
+                </div>
+              );
+            })}
+          </div>
           {layout.map((tab, tabIndex) => {
             return <TabEl tabIndex={tabIndex} tab={tab} />;
           })}
--- src/components/TabPrep/subcomponents/TabEl.js
+++ src/components/TabPrep/subcomponents/TabEl.js
@@ -14,11 +14,7 @@ const TabEl = ({ tab, tabIndex }) => {
   const [activeTab, setActiveTab] = useContext(TabContext);
 
   return (
-    <div className="tab-instance" key={id}>
-      <div className={`tab-title ${ activeTab === tabIndex ? "active-tab" : ''}`}>
-        <h3 onClick={() => setActiveTab(tabIndex)}>{title}</h3>
-      </div>
-      <div className="tab-body">
+      <div className="tab-body" key={id}>
         {activeTab === tabIndex && layout[tabIndex].components.length === 0 ? (
           <Placeholder />
         ) : (
@@ -35,7 +31,6 @@ const TabEl = ({ tab, tabIndex }) => {
           </ul>
         )}
       </div>
-    </div>
   );
 };
 export default TabEl;
