c754912400971f29c027edd879e2e60a2de5781d
Emily <eleblanc@tvo.org>
2022-08-03 16:10:52 -0400

    cleaning up test

--- src/__tests__/Tab.test.js
+++ src/__tests__/Tab.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { unmountComponentAtNode } from "react-dom";
-import { findByText, fireEvent, getByRole, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TabsMain, { ActiveTabProvider } from "../components/Tabs/TabsMain";
-import { LayoutProvider, TabContext } from "../components/Tabs/TabsMain"
+import { LayoutProvider } from "../components/Tabs/TabsMain"
 import Tabs from "../components/Tabs/subcomponents/Tabs";
 
 let container = null;
@@ -64,58 +64,6 @@ describe("Tabs", () => {
     await waitFor(() => {
       expect(screen.getByText(/add a component here/ig)).toBeInTheDocument()
     })
-
-  })
-
-  xit("renders a new tab when add tab btn is pressed", () => { 
-    let addingTabArr = {
-      tabs: [
-        { id: 1, name: "Math", content: [] },
-        {
-          id: 2,
-          name: "Geography",
-          content: [],
-        },
-      ]
-    }
-    render(<TabsMain tabs={addingTabArr.tabs} setProp={(stateUpdate) => addingTabArr = {...addingTabArr, ...stateUpdate}}/>)
-
-    const addTabBtn = screen.getByRole("button", {name: /add tab/i});
-
-    expect(addTabBtn).toBeInTheDocument()
-
-    fireEvent.click(addTabBtn)
-    expect(addingTabArr.tabs).toHaveLength(3)    
-  })
-
-  xit("formatted text and image btns are in the document and fire add component function", () => { 
-
-    const mockFn = jest.fn()
-
-    let addingTabArr = {
-      tabs: [
-        { id: 1, name: "Math", content: [] },
-        {
-          id: 2,
-          name: "Geography",
-          content: [],
-        },
-      ]
-    }
-
-    render(<TabsMain tabs={addingTabArr.tabs} setProp={mockFn}/>)
-
-    const formattedTextBtn = screen.getByRole("button", {name: /add formatted text/i})
-    const imageBtn = screen.getByRole("button", {name: /add image/i})
-
-    expect(formattedTextBtn).toBeInTheDocument()
-    expect(imageBtn).toBeInTheDocument()
-    
-    fireEvent.click(formattedTextBtn)
-    expect(mockFn).toHaveBeenCalled()
-
-    fireEvent.click(imageBtn)
-    expect(mockFn).toHaveBeenCalled()
 
   })
 })
