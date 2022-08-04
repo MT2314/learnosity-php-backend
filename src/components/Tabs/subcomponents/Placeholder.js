443ba2ddf52dc40e3ed5ff90be3ad931cbb7a20b
Emily <eleblanc@tvo.org>
2022-08-04 09:27:14 -0400

    adding period to sentence

--- src/components/Tabs/subcomponents/Placeholder.js
+++ src/components/Tabs/subcomponents/Placeholder.js
@@ -4,7 +4,7 @@ const Placeholder = () => {
   return(
     <div className="placeholder-container">
       <h3>Add a component here!</h3>
-      <p>Drag and drop a component from the left panel or use your keyboard to insert a component</p>
+      <p>Drag and drop a component from the left panel or use your keyboard to insert a component.</p>
       <p>Accepted components: text, image, chart, table, video, and audio. </p>
     </div>
   )
