c947167ca40188105fdadd9afcd76dfda307f5bf
hkava <hkava@tvo.org>
2022-08-04 09:12:30 -0600

    style update and commented out add tab button

--- src/components/Tabs/subcomponents/Tabs.js
+++ src/components/Tabs/subcomponents/Tabs.js
@@ -21,7 +21,7 @@ const Tabs = () => {
 
   return (
     <div className="tab-container">
-      <button
+      {/* <button
         onClick={() =>
           dispatch({
             func: "ADD_TAB",
@@ -31,7 +31,7 @@ const Tabs = () => {
         }
       >
         add tab
-      </button>
+      </button> */}
 
       <div className="tab-title-wrapper">
         {state.map((tabTitle, tabIndex) => {
