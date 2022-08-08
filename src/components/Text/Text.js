8154b0e8fd6e8ef4f382650401bdd904f8230e2f
Sahil Patel <spatel@tvo.org>
2022-08-08 16:29:46 -0400

    Revert "Merge branch 'bugfix/mt-Manual_Mocking' into 'rc-22.0.0'"
    This reverts merge request !300

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,6 +3,11 @@ import DefaultText from "./subcomponent/DefaultText";
 import EditorComponent from "./subcomponent/EditorComponent";
 import "./styles/Text.scss";
 
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+
+//? PP Imports
+import createMFTheme from "../../theme/index";
 import ReactQuillContainer from "../../theme/styledComponents/quillEditor";
 
 export const defaultProps = { body: null };
@@ -11,38 +16,44 @@ const Text = ({ body = { ops: [{ insert: "" }] }, setProp = () => {} }) => {
   const [showEditor, setShowEditor] = useState(false);
   const focusOutofText = useRef(null);
 
+  //* Creating theme
+  const textTheme = createMFTheme();
+
   return (
     <>
-      <ReactQuillContainer>
-        {(!showEditor && body === null) ||
-        (!showEditor && !body.ops) ||
-        (!showEditor && body.ops[0].insert === "") ? (
-          <div
-            onClick={() => {
-              setShowEditor(true);
-            }}
-            className="mainContainer"
-            data-testid="text-component"
-            tabIndex="0"
-            onFocus={() => {
-              setShowEditor(true);
-            }}
-          >
-            <DefaultText />
+      <CssBaseline />
+      <ThemeProvider theme={textTheme}>
+        <ReactQuillContainer>
+          {(!showEditor && body === null) ||
+          (!showEditor && !body.ops) ||
+          (!showEditor && body.ops[0].insert === "") ? (
+            <div
+              onClick={() => {
+                setShowEditor(true);
+              }}
+              className="mainContainer"
+              data-testid="text-component"
+              tabIndex="0"
+              onFocus={() => {
+                setShowEditor(true);
+              }}
+            >
+              <DefaultText />
+            </div>
+          ) : (
+            <EditorComponent
+              body={body}
+              setProp={setProp}
+              setShowEditor={setShowEditor}
+              showEditor={showEditor}
+              focusOutofText={focusOutofText.current}
+            />
+          )}
+          <div className="sr-only" tabIndex="-1" ref={focusOutofText}>
+            Exit Text Component
           </div>
-        ) : (
-          <EditorComponent
-            body={body}
-            setProp={setProp}
-            setShowEditor={setShowEditor}
-            showEditor={showEditor}
-            focusOutofText={focusOutofText.current}
-          />
-        )}
-        <div className="sr-only" tabIndex="-1" ref={focusOutofText}>
-          Exit Text Component
-        </div>
-      </ReactQuillContainer>
+        </ReactQuillContainer>
+      </ThemeProvider>
     </>
   );
 };
