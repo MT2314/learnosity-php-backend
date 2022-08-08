65fc9f1c5bd9e3a1786683fd60ba0eb454d41066
Mike Tkachuk <tkachuk2314@gmail.com>
2022-08-07 21:54:47 -0400

    Theme bug fixed, organized and tooltip override clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { WidgetContextProvider } from "./Utility/mockWrapper";
+// ? Theme Context Provider
+import { ThemeProvider } from "@mui/material/styles";
 // import Header from "./components/Header";
 // import FormattedText from "./components/FormattedText";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AccessibilityKeysPage from "./components/FormattedText/AccessibilityKeysPage";
 import CalloutPage from "./pages/CalloutPage";
@@ -11,14 +13,14 @@ import QuoteBoxPage from "./pages/QuoteBoxPage";
 import ImagePage from "./pages/ImagePage";
 import exposedVersion from "../exposedStage";
 import Text from "./components/Text/Text";
-import { ThemeProvider } from "@mui/material/styles";
-//? PP Imports
+//? Theme Import
 import createMFTheme from "./theme/index";
 
 import "./index.css";
 const App = () => {
   //* Creating theme
   const textTheme = createMFTheme();
+
   console.log(
     `Stage is ${exposedVersion.stage} and version of the app is ${exposedVersion.version}`
   );
