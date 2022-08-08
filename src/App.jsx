72e060bd90356bb233827cfad4850f529266573c
Mike Tkachuk <tkachuk2314@gmail.com>
2022-08-07 21:36:16 -0400

    Theming moved to App.js, Jest Error bug - better anyways

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,38 @@ import QuoteBoxPage from "./pages/QuoteBoxPage";
 import ImagePage from "./pages/ImagePage";
 import exposedVersion from "../exposedStage";
 import Text from "./components/Text/Text";
+import { ThemeProvider } from "@mui/material/styles";
+//? PP Imports
+import createMFTheme from "./theme/index";
 
 import "./index.css";
-
 const App = () => {
-  console.log(`Stage is ${exposedVersion.stage} and version of the app is ${exposedVersion.version}`)
+  //* Creating theme
+  const textTheme = createMFTheme();
+  console.log(
+    `Stage is ${exposedVersion.stage} and version of the app is ${exposedVersion.version}`
+  );
   return (
     <>
       <Suspense fallback="loading">
-        <WidgetContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/AccessibilityKeysPage"
-                element={<AccessibilityKeysPage />}
-              />
-              <Route path="/CalloutPage" element={<CalloutPage />} />
-              <Route path="/QuoteBoxPage" element={<QuoteBoxPage />} />
-              <Route path="/ImagePage" element={<ImagePage />} />
-              <Route path="/text-component" element={<Text />} />
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </BrowserRouter>
-        </WidgetContextProvider>
+        <ThemeProvider theme={textTheme}>
+          <WidgetContextProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                  path="/AccessibilityKeysPage"
+                  element={<AccessibilityKeysPage />}
+                />
+                <Route path="/CalloutPage" element={<CalloutPage />} />
+                <Route path="/QuoteBoxPage" element={<QuoteBoxPage />} />
+                <Route path="/ImagePage" element={<ImagePage />} />
+                <Route path="/text-component" element={<Text />} />
+                <Route path="*" element={<Home />} />
+              </Routes>
+            </BrowserRouter>
+          </WidgetContextProvider>
+        </ThemeProvider>
       </Suspense>
     </>
   );
--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,11 +3,6 @@ import DefaultText from "./subcomponent/DefaultText";
 import EditorComponent from "./subcomponent/EditorComponent";
 import "./styles/Text.scss";
 
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
-
-//? PP Imports
-import createMFTheme from "../../theme/index";
 import ReactQuillContainer from "../../theme/styledComponents/quillEditor";
 
 export const defaultProps = { body: null };
@@ -16,43 +11,37 @@ const Text = ({ body = { ops: [{ insert: "" }] }, setProp = () => {} }) => {
   const [showEditor, setShowEditor] = useState(false);
   const focusOutofText = useRef(null);
 
-  //* Creating theme
-  const textTheme = createMFTheme();
-
   return (
     <>
-      <CssBaseline />
-      <ThemeProvider theme={textTheme}>
-        <ReactQuillContainer>
-          {(!showEditor && body === null) ||
-          (!showEditor && !body.ops) ||
-          (!showEditor && body.ops[0].insert === "") ? (
-            <div
-              onClick={() => {
-                setShowEditor(true);
-              }}
-              className="mainContainer"
-              data-testid="text-component"
-              tabIndex="0"
-              onFocus={() => {
-                setShowEditor(true);
-              }}
-            >
-              <DefaultText />
-            </div>
-          ) : (
-            <EditorComponent
-              body={body}
-              setProp={setProp}
-              setShowEditor={setShowEditor}
-              focusOutofText={focusOutofText.current}
-            />
-          )}
-          <div className="sr-only" tabIndex="-1" ref={focusOutofText}>
-            Exit Text Component
+      <ReactQuillContainer>
+        {(!showEditor && body === null) ||
+        (!showEditor && !body.ops) ||
+        (!showEditor && body.ops[0].insert === "") ? (
+          <div
+            onClick={() => {
+              setShowEditor(true);
+            }}
+            className="mainContainer"
+            data-testid="text-component"
+            tabIndex="0"
+            onFocus={() => {
+              setShowEditor(true);
+            }}
+          >
+            <DefaultText />
           </div>
-        </ReactQuillContainer>
-      </ThemeProvider>
+        ) : (
+          <EditorComponent
+            body={body}
+            setProp={setProp}
+            setShowEditor={setShowEditor}
+            focusOutofText={focusOutofText.current}
+          />
+        )}
+        <div className="sr-only" tabIndex="-1" ref={focusOutofText}>
+          Exit Text Component
+        </div>
+      </ReactQuillContainer>
     </>
   );
 };
