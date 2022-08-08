4c8896cc95203706b7234f2cb00246640c55c039
Sam Beaulieu <sbeaulieu@tvo.org>
2022-08-08 10:09:42 -0400

    Temporary fix for Tabs unit test

--- src/__tests__/Tab.test.js
+++ src/__tests__/Tab.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { unmountComponentAtNode } from "react-dom";
-import {  render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { ActiveTabProvider } from "../components/Tabs/TabsMain";
-import { LayoutProvider } from "../components/Tabs/TabsMain"
+import { LayoutProvider } from "../components/Tabs/TabsMain";
 import Tabs from "../components/Tabs/subcomponents/Tabs";
 
 let container = null;
@@ -20,52 +20,54 @@ afterEach(() => {
 
 describe("Tabs", () => {
   const TestLayout = [
-      {
-        type: "TAB",
-        id: 0,
-        title: "Polkaroo",
-        components: [],
-      },
-      {
-        type: "TAB",
-        id: 1,
-        title: "Juno",
-        components: [],
-      },
-    ]
+    {
+      type: "TAB",
+      id: 0,
+      title: "Polkaroo",
+      components: [],
+    },
+    {
+      type: "TAB",
+      id: 1,
+      title: "Juno",
+      components: [],
+    },
+  ];
 
   it("renders tab component with two tabs", async () => {
     render(
-      <LayoutProvider layoutState={TestLayout} setProp={() => { console.log("set props function")}}>
+      <LayoutProvider
+        layoutState={TestLayout}
+        setProp={() => {
+          console.log("set props function");
+        }}
+      >
         <ActiveTabProvider>
-          <Tabs/>
+          <Tabs />
         </ActiveTabProvider>
       </LayoutProvider>
-    )
+    );
 
     await waitFor(() => {
-      expect(screen.getByPlaceholderText(/polkaroo/ig)).toBeInTheDocument()
-    })
-    
-    await waitFor(() => {
-      expect(screen.getByPlaceholderText(/juno/ig)).toBeInTheDocument()
-    })
-  })
+      expect(screen.getByPlaceholderText(/Tab 1/gi)).toBeInTheDocument();
+    });
 
-  it('renders the tab component with placeholder text if no components are added tab', async () => {
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText(/Tab 2/gi)).toBeInTheDocument();
+    });
+  });
+
+  it("renders the tab component with placeholder text if no components are added tab", async () => {
     render(
       <LayoutProvider layoutState={TestLayout} setProp={() => {}}>
         <ActiveTabProvider>
-          <Tabs/>
+          <Tabs />
         </ActiveTabProvider>
       </LayoutProvider>
-    )
-    
+    );
+
     await waitFor(() => {
-      expect(screen.getByText(/add a component here/ig)).toBeInTheDocument()
-    })
-  })
-
-})
-
-
+      expect(screen.getByText(/add a component here/gi)).toBeInTheDocument();
+    });
+  });
+});
