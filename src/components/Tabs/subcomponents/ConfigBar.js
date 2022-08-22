30cc69eb43b401ba4ac7f90579e3a7160a17103a
Mike Tkachuk <tkachuk2314@gmail.com>
2022-08-22 15:08:58 -0400

    styling config, min-height issue-can't access

--- src/components/Tabs/subcomponents/ConfigBar.js
+++ src/components/Tabs/subcomponents/ConfigBar.js
@@ -1,8 +1,10 @@
-import React, { createContext, useContext, useState, useCallback } from "react";
+import React, { useContext, useState, useCallback } from "react";
 import styled from "@emotion/styled";
 import { LayoutContext, TabContext } from "../TabContext";
 import AppBar from "@material-ui/core/AppBar";
 import { Toolbar as MUIToolbar } from "@material-ui/core/";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+
 import DialogProvider from "../../../Utility/DialogProvider";
 
 // ? Icons
@@ -13,37 +15,67 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 
+// * Styled Components
+// ? Styled Container for configBar
 const Container = styled("div")({
   display: "absolute",
-  width: "184px",
   color: "white",
-  margin: "auto",
 });
 
+// ? Styled Tooltip, differnet but most compact method for styling tooltip
+const StyledTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "rgba(97, 97, 97, 0.9)",
+    border: "4px",
+    color: theme.palette.common.white,
+    height: "22px",
+    padding: "4px, 8px, 4px, 8px",
+    fontSize: "10px",
+    lineHeight: "14px",
+    fontWeight: "500",
+    "& .MuiTooltip-arrow": {
+      color: "rgba(97, 97, 97, 0.9)",
+    },
+  },
+}));
+
+// ? styled Toolbar
 const Toolbar = styled(MUIToolbar)({
   position: "relative",
+
   display: "flex",
-  justifyContent: "space-between",
-  width: "184px",
+  justifyContent: "space-evenly",
+  width: "146px",
   height: "40px",
   color: "#000",
   boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
   borderLeft: "4px solid #1565c0",
   backgroundColor: "white",
-  borderRadius: "4px",
-  padding: "8px,10px",
+  margin: "10px, 8px",
+
+  "& .MuiToolbar-gutters": {
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
 });
 
+// ? styled IconButton
 const IconButton = styled(MUIIconButton)({
   width: "30px",
   height: "30px",
   padding: "7px",
+  marginLeft: "4px",
   color: "#000",
-  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  backgroundColor: "none",
   borderLeft: "4px solid #1565c0",
-  borderRadius: "4px",
+  borderRadius: "0!important",
   "&:hover": {
-    backgroundColor: "rgb(226, 236, 245)",
+    backgroundColor: "rgba(21, 101, 192, 0.12)",
+    color: "#1565C0",
+
+    borderRadius: "0",
   },
 });
 
@@ -132,48 +164,52 @@ const ConfigBar = () => {
   return (
     <Container>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            disabled={activeTab === 0}
-            onClick={() => moveTabLeft(state, activeTab)}
-          >
-            <ArrowBackIcon />
-          </IconButton>
-          <IconButton
-            edge="start"
-            color="inherit"
-            disabled={activeTab >= state.length - 1}
-            onClick={() => moveTabRight(state, activeTab)}
-          >
-            <ArrowForwardIcon />
-          </IconButton>
-          <IconButton
-            edge="start"
-            color="inherit"
-            disabled={state.length >= 4}
-            onClick={() => {
-              addTab(state, activeTab);
-            }}
-          >
-            <AddIcon />
-          </IconButton>
+        <Toolbar variant="dense" disableGutters>
+          <StyledTooltip title="move tab left" arrow placement="top">
+            <IconButton
+              color="inherit"
+              disabled={activeTab === 0}
+              onClick={() => moveTabLeft(state, activeTab)}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+          </StyledTooltip>
+          <StyledTooltip title="move tab right" arrow placement="top">
+            <IconButton
+              color="inherit"
+              disabled={activeTab >= state.length - 1}
+              onClick={() => moveTabRight(state, activeTab)}
+            >
+              <ArrowForwardIcon />
+            </IconButton>
+          </StyledTooltip>
+          <StyledTooltip title="add tab" arrow placement="top">
+            <IconButton
+              color="inherit"
+              disabled={state.length >= 4}
+              onClick={() => {
+                addTab(state, activeTab);
+              }}
+            >
+              <AddIcon />
+            </IconButton>
+          </StyledTooltip>
           {showDialog && (
             <DialogProvider initialState={removeTabDialog} defaultState />
           )}
-          <IconButton
-            edge="start"
-            color="inherit"
-            disabled={state.length <= 2}
-            onClick={() => {
-              state[activeTab].components.length > 0
-                ? setShowDialog(true)
-                : removeTab(state, activeTab);
-            }}
-          >
-            <RemoveIcon />
-          </IconButton>
+          <StyledTooltip title="remove current tab" arrow placement="top">
+            <IconButton
+              color="inherit"
+              disabled={state.length <= 2}
+              onClick={() => {
+                state[activeTab].components.length > 0
+                  ? setShowDialog(true)
+                  : removeTab(state, activeTab);
+              }}
+            >
+              <RemoveIcon />
+            </IconButton>
+          </StyledTooltip>
         </Toolbar>
       </AppBar>
     </Container>
