dd67fbb7fb093cc9bc7954baefe3d3d756a99376
Emily <eleblanc@tvo.org>
2022-08-24 16:01:40 -0400

    added configuration for css modules

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  verbose:true,
-  transformIgnorePatterns: [
-    "/node_modules/(?!react-dnd|core-dnd|@react-dnd|dnd-core|react-dnd-html5-backend)"
-    ]
-}
+// const config = {
+//   verbose: true,
+//   testEnvironment: 'jest-environment-jsdom',
+//   transformIgnorePatterns: [
+//     '/node_modules/(?!(react-dnd|dnd-core|@react-dnd|dnd-core|react-dnd-html5-backend|react-dnd-test-utils|react-dnd-test-backend)/)',
+//   ],
+// }
+// module.exports = config
