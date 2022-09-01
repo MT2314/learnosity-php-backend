813c5f7713d53e4ae85c91cd428fe2575c22a052
Wilson <wruan@tvo.org>
2022-09-01 12:26:40 -0400

    code clean up

--- src/components/Tabs/subcomponents/Placeholder.js
+++ src/components/Tabs/subcomponents/Placeholder.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 
-const PlaceholderContainer = styled("div")(({ isOver }) => ({
-  backgroundColor: isOver ? "rgba(21, 101, 192, 0.04)" : "inherit",
+const PlaceholderContainer = styled("div")(({ isOver, showError }) => ({
+  backgroundColor: isOver ? (showError ? "rgba(211, 47, 47, 0.04)" : "rgba(21, 101, 192, 0.04)") : "inherit",
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
   height: "130px",
-  border: "3px dashed #1565c0",
-}))
-
-const PlaceholderHover = styled(PlaceholderContainer)(({ isOver }) => ({
-  backgroundColor: isOver ? "rgba(21, 101, 192, 0.04)" : "rgba(211, 47, 47, 0.04)",
-  border: isOver ? "3px dashed #1565c0" : "3px dashed #D32F2F",
+  border: showError ? "3px dashed #D32F2F" : "3px dashed #1565c0",
 }))
 
 const Title = styled("h3")(({ isOver }) => ({
@@ -35,31 +30,21 @@ const SubParagraph = styled(Paragraph)({
   color: "#1565c0",
 })
 const Placeholder = ({ isOver, getItem }) => {
-  const [stayHover, setStayHover] = useState()
+  const [showError, setShowError] = useState()
   useEffect(() => {
     if (isOver && (getItem.componentName != 'Text' | 'Table' | 'Video' | 'Image')) {
-      setStayHover(getItem.componentName);
+      setShowError(getItem.componentName);
     } else if (isOver) {
-      setStayHover();
+      setShowError();
     }
   }, [isOver])
 
   return (
-    <>
-      {stayHover ?
-        <PlaceholderHover isOver={!stayHover}>
-          {!stayHover ? <Title>Add a component here!</Title> : <Title isOver={true}>{`Error: ${stayHover} not complatible!`}</Title>}
-          <Paragraph>Drag and drop a component from the left panel or use your keyboard to insert a component.</Paragraph>
-          <SubParagraph>Accepted components: text, image, chart, table, video, and audio. </SubParagraph>
-        </PlaceholderHover>
-        :
-        <PlaceholderContainer isOver={isOver}>
-          <Title>Add a component here!</Title>
-          <Paragraph>Drag and drop a component from the left panel or use your keyboard to insert a component.</Paragraph>
-          <SubParagraph>Accepted components: text, image, chart, table, video, and audio. </SubParagraph>
-        </PlaceholderContainer>
-      }
-    </>
+    <PlaceholderContainer isOver={isOver} showError={showError}>
+      {showError ? <Title>{`Error: ${showError} not complatible!`}</Title> : <Title>Add a component here!</Title>}
+      <Paragraph>Drag and drop a component from the left panel or use your keyboard to insert a component.</Paragraph>
+      <SubParagraph>Accepted components: text, image, chart, table, video, and audio. </SubParagraph>
+    </PlaceholderContainer>
   )
 
 }
