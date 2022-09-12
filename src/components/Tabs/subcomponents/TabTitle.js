82919a057a91c85121972e5b48d9df9bf97b8d87
Emily <eleblanc@tvo.org>
2022-09-12 17:51:05 -0400

    tab styles

--- src/components/Tabs/subcomponents/TabTitle.js
+++ src/components/Tabs/subcomponents/TabTitle.js
@@ -1,6 +1,51 @@
 import React, { useContext, useCallback, useRef } from "react";
 import { TabContext, LayoutContext } from "../TabContext";
 import { TextareaAutosize } from "@material-ui/core";
+import styled from "@emotion/styled";
+
+//TODO: add styles to active tab
+// border-bottom: 0px;
+// z-index: 1;
+
+const StyledTitle = styled('div')(({ theme, activeTab, tabIndexProp }) => (
+  {
+    backgroundColor: activeTab === tabIndexProp ? '#fff' : '#f5f5f5',
+    border: '1px solid #bdbdbd',
+    borderRadius: '10px 10px 0px 0px',
+    width: '100%',
+    maxWidth: '484px',
+    padding: '8px 10px',
+    color: activeTab === tabIndexProp ? '#232323' :'#636363',
+    fontWeight: '500',
+    textAlign: 'center',
+    '&:focus': {
+      outline: '1px solid black',
+      border: '1px solid black',
+    },
+    '&:focus-visible': {
+      outline: '1px solid black',
+      border: '1px solid black',
+    }
+  }
+))
+
+const StyledPlaceholder = styled('div')(({ theme, activeTab, tabIndexProp }) => (
+  {
+    width: '100%',
+    maxWidth: '484px',
+    maxHeight: '50px',
+    padding: '0',
+    margin: '0',
+    fontSize: '18px',
+    wordWrap: 'break-word',
+    overflowX: 'hidden',
+    overflowY: 'hidden',
+    textOverflow: 'ellipsis',
+    display: '-webkit-box',
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: activeTab == tabIndexProp ? "unset" : 2,
+  }
+))
 
 const TabTitle = ({ tabTitle, tabIndex, showToolbar, placeholderTitle }) => {
   const [activeTab, setActiveTab] = useContext(TabContext);
@@ -31,11 +76,12 @@ const TabTitle = ({ tabTitle, tabIndex, showToolbar, placeholderTitle }) => {
   };
 
   return (
-    <div
+    <StyledTitle
+      activeTab={activeTab}
+      tabIndexProp={tabIndex}
       key={`tab-title-${tabIndex}`}
       tabIndex="0"
       aria-label={tabTitle ? tabTitle : `Untitled ${placeholderTitle}`}
-      className={`tab-title ${activeTab === tabIndex ? "active-tab" : ""}`}
       onFocus={() => {
         showToolbar(true);
       }}
@@ -72,17 +118,15 @@ const TabTitle = ({ tabTitle, tabIndex, showToolbar, placeholderTitle }) => {
           ref={inputRef}
         />
       ) : (
-        <p
-          className="placeholder-title"
+        <StyledPlaceholder
+          activeTab={activeTab}
+          tabIndexProp={tabIndex}
           role="tab"
-          style={{
-            WebkitLineClamp: activeTab == tabIndex ? "unset" : 2,
-          }}
         >
           {tabTitle ? tabTitle : placeholderTitle}
-        </p>
+        </StyledPlaceholder>
       )}
-    </div>
+    </StyledTitle>
   );
 };
 export default TabTitle;
