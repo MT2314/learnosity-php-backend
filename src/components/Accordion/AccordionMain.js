0373ab5d1f2caea2044173550bc6cf3bd6857053
Sahil <spatel@tvo.org>
2022-09-14 09:02:58 -0400

    Added accordion from mui:

--- src/components/Accordion/AccordionMain.js
+++ src/components/Accordion/AccordionMain.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { v4 as uuidv4 } from "uuid";
+//Accordion Imports
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 //Accordion default props
 export const defaultProps = {
@@ -13,7 +20,26 @@ export const defaultProps = {
 };
 
 const AccordionMain = () => {
-    return <h2>I am Accordion</h2>
+    return (
+        <div>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+
+    )
 }
 
 export default AccordionMain;
