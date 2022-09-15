5dac9a5d147fab6df0fb26a5cb985cfc113c51b2
Emily <eleblanc@tvo.org>
2022-09-15 15:18:59 -0400

    item drops

--- src/components/Accordion/subComponent/AccordionItem.js
+++ src/components/Accordion/subComponent/AccordionItem.js
@@ -1,11 +1,46 @@
 import React from "react"
 import PlaceHolder from '../subComponent/PlaceHolder'
+import { useDrop } from "react-dnd";
 
 const AccordionItem = ({accordion, accordionIndex}) => {
-
     const {components} = accordion
+
+    const [{ isOver, getItem }, drop] = useDrop(() => ({
+        accept: [
+          "Text",
+          "Image",
+          "Video",
+          "Table",
+          "InfoBox",
+          "QuoteBox",
+          "IFrame",
+        ],
+        drop: async (item, monitor) => {
+            console.log("DROPPED ITEM:", item)
+        //   if (!acceptListComp(item)) setShowDropError(true);
+        //   if (item.within && components.length !== 0) return;
+        //   if (monitor.didDrop()) return;
+        //   if (acceptListComp(item)) {
+        //     dispatch({
+        //       func: "ADD_COMPONENT",
+        //       tabIndex: tabIndex,
+        //       component: {
+        //         componentName: item.componentName,
+        //         componentProps: JSON.parse(item?.componentProps),
+        //       },
+        //     });
+        //     item?.delete && item?.delete(item.tabIndex, item.compIndex);
+        //   }
+        },
+    
+        collect: (monitor) => ({
+          isOver: !!monitor.isOver(),
+          getItem: monitor.getItem(),
+        }),
+      }));
+
     return (
-        <div style={{border:"2px solid red"}}>
+        <div ref={drop} style={{ background: isOver && 'green'}}>
             {
             components ?
              components.map((component, compIndex) => (
