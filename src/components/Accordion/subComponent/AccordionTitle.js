57f030782b0fa46431de14257efd684893b0f392
Emily <eleblanc@tvo.org>
2022-09-16 13:35:52 -0400

    max length added

--- src/components/Accordion/subComponent/AccordionTitle.js
+++ src/components/Accordion/subComponent/AccordionTitle.js
@@ -1,33 +1,34 @@
 import React, { useContext, useCallback } from 'react'
 import { LayoutContext } from '../../../Context/InteractivesContext'
-import { TextField } from "@material-ui/core";
-import styled from '@emotion/styled';
+import { TextField} from "@material-ui/core";
 
 const AccordionTitle = ({ accordionTitle, accordionIndex, placeholderTitle }) => {
 
-    const [state, dispatch] = useContext(LayoutContext)
-
+    const [, dispatch] = useContext(LayoutContext)
 
     const handleTitleChange = useCallback((e) => {
-        dispatch({
-          func: "CHANGE_TITLE",
-          title: e.target.value,
-          layerIndex: accordionIndex,
-        });
-      }, []);
+        if( e.target.value.length < 10){
+           dispatch({
+               func: "CHANGE_TITLE",
+               title: e.target.value,
+               layerIndex: accordionIndex,
+           });
+        }return
+    }, []);
 
     return (
-            <TextField
-                key={`accordion-title-${accordionIndex}`}
-                accordionTitle={accordionTitle}
-                aria-label={accordionTitle ? accordionTitle : `Untitled ${placeholderTitle}`}
-                fullWidth={true}
-                id={`textfield-${accordionIndex}`}
-                placeholder={placeholderTitle}
-                value={accordionTitle}
-                InputProps={{ disableUnderline: true }}
-                onChange={handleTitleChange}
-             />
+        <TextField
+            key={`accordion-title-${accordionIndex}`}
+            accordionTitle={accordionTitle}
+            aria-label={accordionTitle ? accordionTitle : `Untitled ${placeholderTitle}`}
+            fullWidth={true}
+            id={`textfield-${accordionIndex}`}
+            placeholder={placeholderTitle}
+            value={accordionTitle}
+            InputProps={{ disableUnderline: true}}
+            onChange={handleTitleChange}
+            maxlength="5"
+        />
     )
 }
 
