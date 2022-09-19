5aaebc37b643800c54a38d2647a196235977d053
hkava <hkava@tvo.org>
2022-09-19 12:31:11 -0600

    image mock data

--- amp-template-generator/src/views/data/imageComponent.js
+++ amp-template-generator/src/views/data/imageComponent.js
@@ -28,7 +28,7 @@ module.exports = async function () {
           {
             __typename: "CourseStructureContainer",
             id: "6318a3c78f3a72b1dc77a9f0",
-            name: "Text Component",
+            name: "Image Component",
             type: "lesson",
             componentContainers: [
               {
@@ -51,22 +51,10 @@ module.exports = async function () {
                     type: "LEARNING",
                     components: [
                       {
-                        componentName: "Text",
+                        componentName: "Image",
                         props:
-                          '{"body":{"ops":[{"insert":"Think about what a bear would need to be healthy in their environment and draw some examples.\\n\\nHint: Think about what the bear needs.\\n\\nComplete the "},{"attributes":{"bold":true},"insert":"Bear Environment Activity"},{"insert":" in your notebook or using the following fillable and printable document. If you would like, you can use speech-to-text or audio recording tools to record your thoughts.\\n"}]}}',
-                        position: "U",
-                      },
-                      {
-                        componentName: "Text",
-                        props:
-                          '{"body":{"ops":[{"insert":"Sharing our learning\\n\\nHow can you share your learning about how environments help animals and people meet their needs?\\n\\nWhat is the best way for you to show someone else this important information?\\nDo you enjoy sharing using pictures?"},{"attributes":{"list":"bullet"},"insert":"\\n"},{"insert":"Do you enjoy written words?"},{"attributes":{"list":"bullet"},"insert":"\\n"},{"insert":"Do you enjoy sharing using spoken words?"},{"attributes":{"list":"bullet"},"insert":"\\n"},{"insert":"Could you make a video?"},{"attributes":{"list":"bullet"},"insert":"\\n"},{"insert":"\\n"}]}}',
+                          '{"imgSize":"default","uploadedImg":"https://cdn.shopify.com/s/files/1/0023/1794/2820/files/Polkaroo_large.PNG?v=1556241987","imgLink":"https://www.tvo.org","alt":"Polkaroo standing with crossed arms","longDesc":"","caption":"I am a caption","credit":null}',
                         position: "g",
-                      },
-                      {
-                        componentName: "Text",
-                        props:
-                          '{"body":{"ops":[{"insert":"Reflection\\n\\nHow do you feel about what you have learned in this activity? Which of the next four sentences best matches how you are feeling about your learning? Press the button that is beside this sentence.\\n"}]}}',
-                        position: "p",
                       },
                     ],
                   },
