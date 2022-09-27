4b11fd0d87f171a113a0ece1e29e2cb8f2c96c36
Mike Tkachuk <tkachuk2314@gmail.com>
2022-09-27 10:53:44 -0400

    img aria

--- src/components/InfoBox/subcomponents/Icon.js
+++ src/components/InfoBox/subcomponents/Icon.js
@@ -2,9 +2,11 @@ import React, { useEffect, useContext, useMemo } from "react";
 
 import { InfoBoxContext } from "../InfoBoxContext";
 import { iconDropdownOptions, defaultIcon } from "../icons/infoBoxIcons";
+import { useTranslation } from "react-i18next";
 
 const Icon = ({ setSelectedIcon, selectedIcon }) => {
   const [state] = useContext(InfoBoxContext);
+  const { t } = useTranslation();
 
   const stateIcon = useMemo(() => state?.infoBoxIcon, [state?.infoBoxIcon]);
 
@@ -13,7 +15,11 @@ const Icon = ({ setSelectedIcon, selectedIcon }) => {
   }, []);
 
   return (
-    <div data-icon={stateIcon ? stateIcon : ""} data-testid="icon">
+    <div
+      data-icon={stateIcon ? stateIcon : ""}
+      data-testid="icon"
+      aria-label={t(`${selectedIcon}`)}
+    >
       {stateIcon !== null
         ? iconDropdownOptions.find((icon) => icon.type === stateIcon)?.icon
         : defaultIcon}
