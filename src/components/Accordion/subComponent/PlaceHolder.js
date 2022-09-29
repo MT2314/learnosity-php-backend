8465cb119cb4540729ef024f9005c41860d02540
Sam Beaulieu <sbeaulieu@tvo.org>
2022-09-29 09:45:03 -0400

    Adjusted padding around blue border in placeholder to reflect design

--- src/components/Accordion/subComponent/PlaceHolder.js
+++ src/components/Accordion/subComponent/PlaceHolder.js
@@ -2,12 +2,17 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const PlaceholderContainer = styled("div")({
+  padding: "0.375rem 0.125rem 0.125rem 0.125rem",
+});
+
+const PlaceholderOutline = styled("div")({
+  height: "130px",
+  width: "100%",
+  border: "3px dashed #1565c0",
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-  height: "130px",
-  border: "3px dashed #1565c0",
 });
 
 const Title = styled("h3")(() => ({
@@ -33,14 +38,16 @@ const SubParagraph = styled(Paragraph)({
 const Placeholder = () => {
   return (
     <PlaceholderContainer>
-      <Title>Add a component here!</Title>
-      <Paragraph>
-        Drag and drop a component from the left panel or use your keyboard to
-        insert a component.
-      </Paragraph>
-      <SubParagraph>
-        Accepted components: text, image, chart, table, video, and audio.
-      </SubParagraph>
+      <PlaceholderOutline>
+        <Title>Add a component here!</Title>
+        <Paragraph>
+          Drag and drop a component from the left panel or use your keyboard to
+          insert a component.
+        </Paragraph>
+        <SubParagraph>
+          Accepted components: text, image, chart, table, video, and audio.
+        </SubParagraph>
+      </PlaceholderOutline>
     </PlaceholderContainer>
   );
 };
