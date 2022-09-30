9c6e828eef99d09653aea86cf4a802a7ca9564d7
Emily <eleblanc@tvo.org>
2022-09-30 14:40:30 -0400

    added drag label to text video image

---
+++ src/Utility/DragLabel.js
@@ -0,0 +1,100 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Menu } from '@mui/icons-material';
+import { useDragLayer } from "react-dnd";
+
+const StyledLabel = styled("div")(({ theme }) => ({
+  display:'flex',
+  alignItems: 'center',
+  width: 'fit-content',
+  height: '64px',
+  padding: '18px 20px 17px 20px',
+  background: '#FFFFFF',
+  border: '3px solid #1565C0',
+  borderRadius: '4px',
+}));
+
+const StyledTypography = styled('p')(({theme}) => ({
+  fontFamily: 'Inter',
+  fontWeight: 500,
+  fontSize: '18px',
+  lineHeight: '160%',
+  letterSpacing: '0.15px',
+  color: '#000000',
+  margin: '0px 0px 0px 10px',
+}))
+
+const StyledlayerStyles = styled('div')(() => ({
+  position: "fixed",
+  pointerEvents: "none",
+  zIndex: 100,
+  left: 0,
+  top: 0,
+  width: "100%",
+  height: "100%",
+}));
+
+
+
+const DragLabel = () => { 
+  const { 
+    item,
+    isDragging,
+    initialCursorOffset,
+    initialFileOffset,
+    currentFileOffset,} = useDragLayer(
+    monitor => ({
+      item: monitor.getItem(),
+      itemType: monitor.getItemType(),
+      initialCursorOffset: monitor.getInitialClientOffset(),
+      initialFileOffset: monitor.getInitialSourceClientOffset(),
+      currentFileOffset: monitor.getSourceClientOffset(),
+      isDragging: monitor.isDragging(),
+    }))
+
+  if (!isDragging) {
+    return null;
+  }
+
+  return (
+    <StyledlayerStyles>
+      <div
+        style={getItemStyles(
+          initialCursorOffset,
+          initialFileOffset,
+          currentFileOffset
+        )}
+      >
+      <div>
+      <StyledLabel>
+        <Menu/>
+        <StyledTypography>{item.componentName}</StyledTypography>
+      </StyledLabel>
+      </div>
+      </div>
+    </StyledlayerStyles>
+  );
+
+  
+  function getItemStyles(
+    initialCursorOffset,
+    initialOffset,
+    currentOffset,
+  ) {
+    if (!initialOffset || !currentOffset || !initialCursorOffset) {
+      return {
+        display: "none",
+      };
+    }
+  
+    const x = initialCursorOffset?.x + (currentOffset.x - initialOffset.x);
+    const y = initialCursorOffset?.y + (currentOffset.y - initialOffset.y);
+    const transform = `translate(${x}px, ${y}px)`;
+  
+    return {
+      transform,
+      WebkitTransform: transform,
+    };
+  }
+}  
+export default DragLabel;
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FormattedText from "../FormattedText/FormattedText";
+import DragLabel from "../../Utility/DragLabel";
 import { useToolBarOptions } from "../../hooks/useToolBarOptions";
 
 import YouTube from "react-youtube";
@@ -28,51 +29,55 @@ const Video = ({
   // );
 
   return (
-    <div className={styles.videoContainer} data-testid="video">
-      {type === "" ||
-      (type === "brightcove" && !videoId) ||
-      (type === "youTube" && !videoId) ? (
-        <div className={styles.placeholderImg} tabIndex="0"></div>
-      ) : type === "youTube" && videoId ? (
-        <div className={styles.youTubePlayer} data-testid="youTubePlayer">
-          <YouTube videoId={videoId} />
-        </div>
-      ) : type === "brightcove" && videoId ? (
-        <div className={styles.brightcovePlayer} data-testid="brightcovePlayer">
-          <ReactPlayerLoader accountId="23648095001" videoId={videoId} />
-        </div>
-      ) : null}
-      {(type === "youTube" && videoId) || (type === "brightcove" && videoId) ? (
-        <>
-          <div
-            className={styles.transcriptContainer}
-            data-testid="videoTranscript"
-          >
-            <FormattedText
-              placeHolderText="Type transcript here..."
-              body={transcript}
-              setProp={(stateUpdate) =>
-                setProp({ transcript: stateUpdate.body })
-              }
-            />
+    <>
+      {/* on drag the drag label appears with component name */}
+      <DragLabel />
+      <div className={styles.videoContainer} data-testid="video">
+        {type === "" ||
+          (type === "brightcove" && !videoId) ||
+          (type === "youTube" && !videoId) ? (
+          <div className={styles.placeholderImg} tabIndex="0"></div>
+        ) : type === "youTube" && videoId ? (
+          <div className={styles.youTubePlayer} data-testid="youTubePlayer">
+            <YouTube videoId={videoId} />
           </div>
-          <div className={styles.captionContainer} data-testid="videoCaption">
-            <FormattedText
-              placeHolderText="Type caption here..."
-              body={caption}
-              setProp={(stateUpdate) => setProp({ caption: stateUpdate.body })}
-            />
+        ) : type === "brightcove" && videoId ? (
+          <div className={styles.brightcovePlayer} data-testid="brightcovePlayer">
+            <ReactPlayerLoader accountId="23648095001" videoId={videoId} />
           </div>
-          <div className={styles.creditContainer} data-testid="videoCredit">
-            <FormattedText
-              placeHolderText="Type credit here..."
-              body={credit}
-              setProp={(stateUpdate) => setProp({ credit: stateUpdate.body })}
-            />
-          </div>
-        </>
-      ) : null}
-    </div>
+        ) : null}
+        {(type === "youTube" && videoId) || (type === "brightcove" && videoId) ? (
+          <>
+            <div
+              className={styles.transcriptContainer}
+              data-testid="videoTranscript"
+            >
+              <FormattedText
+                placeHolderText="Type transcript here..."
+                body={transcript}
+                setProp={(stateUpdate) =>
+                  setProp({ transcript: stateUpdate.body })
+                }
+              />
+            </div>
+            <div className={styles.captionContainer} data-testid="videoCaption">
+              <FormattedText
+                placeHolderText="Type caption here..."
+                body={caption}
+                setProp={(stateUpdate) => setProp({ caption: stateUpdate.body })}
+              />
+            </div>
+            <div className={styles.creditContainer} data-testid="videoCredit">
+              <FormattedText
+                placeHolderText="Type credit here..."
+                body={credit}
+                setProp={(stateUpdate) => setProp({ credit: stateUpdate.body })}
+              />
+            </div>
+          </>
+        ) : null}
+      </div>
+    </>
   );
 };
 
