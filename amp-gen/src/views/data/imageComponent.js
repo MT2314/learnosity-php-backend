90235d6754907436075d18d8b597e73347c7f59d
hkava <hkava@tvo.org>
2022-10-03 10:23:22 -0600

    updated all mock data schemas

---
+++ amp-gen/src/views/data/imageComponent.js
@@ -0,0 +1,75 @@
+const convertData = require("../utils/convert-data");
+
+module.exports = async function () {
+  const data = [
+    {
+      __typename: "LessonStructureContainer",
+      id: "633aebff203363a7d97f7354",
+      type: "lesson",
+      name: "Image Component",
+      description: null,
+      indexInParent: 0,
+      componentContainer: {
+        __typename: "ComponentContainer",
+        id: "633aebff203363a7d97f7355",
+        lastUpdateID: "46430",
+        title: "Component",
+        sections: [
+          {
+            __typename: "Section",
+            id: "633aebff203363a7d97f7356",
+            type: "NONLEARNING",
+            locked: true,
+            components: [
+              {
+                id: "633aebff203363a7d97f7357",
+                componentName: "Text",
+                props: '{"text":"default text"}',
+                locked: true,
+              },
+            ],
+          },
+          {
+            __typename: "Section",
+            id: "633aebff203363a7d97f7358",
+            type: "LEARNING",
+            locked: false,
+            components: [
+              {
+                componentName: "Image",
+                props:
+                  '{"imgSize":"default","uploadedImg":"https://cdn.shopify.com/s/files/1/0023/1794/2820/files/Polkaroo_large.PNG?v=1556241987","imgLink":"https://www.tvo.org","alt":"Polkaroo standing with crossed arms","longDesc":"","caption":"I am a caption","credit":null}',
+                locked: false,
+              },
+            ],
+          },
+        ],
+        parentCourseID: "633aebff203363a7d97f7352",
+        createdAt: "1664805887360",
+        updatedAt: "1664811470175",
+      },
+      path: [
+        {
+          id: "633aebff203363a7d97f7353",
+          type: "unit",
+          name: "AMP Components",
+          indexInParent: 0,
+        },
+      ],
+      index: 1,
+    },
+  ];
+
+  if (data?.__typename === "Course") {
+    convertData.dataConversionFunction(data);
+  } else if (
+    // If the returns is an array of LessonStructureContainers (getLessons)
+    Array.isArray(data) &&
+    data[0]?.__typename === "LessonStructureContainer"
+  ) {
+    convertData.dataConversionFlatArray(data);
+  } else {
+    // TODO: may need to check for type if __typename does not exist eg. getLesson
+  }
+  return convertData.lessons;
+};
