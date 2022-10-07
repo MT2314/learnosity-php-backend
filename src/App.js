f5642fd6b9db9e3d1308b468a65c96550fa4b551
Sahil <spatel@tvo.org>
2022-10-07 10:39:01 -0400

    Added  DndProvider

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { WidgetContextProvider } from "./Utility/mockWrapper";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 // import Header from "./components/Header";
 // import FormattedText from "./components/FormattedText";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AccessibilityKeysPage from "./components/FormattedText/AccessibilityKeysPage";
 import CalloutPage from "./pages/CalloutPage";
@@ -21,24 +23,26 @@ const App = () => {
   );
   return (
     <>
-      <Suspense fallback="loading">
-        <WidgetContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/AccessibilityKeysPage"
-                element={<AccessibilityKeysPage />}
-              />
-              <Route path="/CalloutPage" element={<CalloutPage />} />
-              <Route path="/QuoteBoxPage" element={<QuoteBoxPage />} />
-              <Route path="/ImagePage" element={<ImagePage />} />
-              <Route path="/text-component" element={<Text />} />
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </BrowserRouter>
-        </WidgetContextProvider>
-      </Suspense>
+      <DndProvider backend={HTML5Backend}>
+        <Suspense fallback="loading">
+          <WidgetContextProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                  path="/AccessibilityKeysPage"
+                  element={<AccessibilityKeysPage />}
+                />
+                <Route path="/CalloutPage" element={<CalloutPage />} />
+                <Route path="/QuoteBoxPage" element={<QuoteBoxPage />} />
+                <Route path="/ImagePage" element={<ImagePage />} />
+                <Route path="/text-component" element={<Text />} />
+                <Route path="*" element={<Home />} />
+              </Routes>
+            </BrowserRouter>
+          </WidgetContextProvider>
+        </Suspense>
+      </DndProvider>
     </>
   );
 };
