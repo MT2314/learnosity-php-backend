8f333081062aa612a6210ac58820825a99d29c6a
Sam Beaulieu <sbeaulieu@tvo.org>
2022-10-18 15:31:39 -0400

    Deleted unused/unnecessary code

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -7,7 +7,6 @@ import "./styles/Text.scss";
 import PopupDialogs from "./dialogs/PopupDialogs";
 
 import { CssBaseline } from "@mui/material";
-import DragLabel from "../../Utility/DragLabel";
 // import { ThemeProvider } from "@mui/material/styles";
 
 //? PP Imports
@@ -48,8 +47,6 @@ const Text = ({
 
   return (
     <>
-      {/* on drag <DragLabel/> shows the components name */}
-      <DragLabel />
       <CssBaseline />
       {/* <ThemeProvider theme={textTheme}> */}
       <ReactQuillContainer isInfoBox={isInfoBox} isVideo={isVideo}>
