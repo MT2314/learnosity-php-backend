3ba60a8e4c69e784e7a96923388c353b8a0d272b
Wilson <wruan@tvo.org>
2022-10-20 15:14:37 -0400

    working on table

---
+++ amp-template-generator/src/mockData/componentTable.js
@@ -0,0 +1,72 @@
+const convertData = require("../utils/convert-data");
+
+module.exports = async function () {
+    const data = [
+        {
+          __typename: "LessonStructureContainer",
+          id: "633aebff203363a7d97f7354",
+          type: "lesson",
+          name: "Table Component",
+          description: null,
+          indexInParent: 0,
+          componentContainer: {
+            __typename: "ComponentContainer",
+            id: "633aebff203363a7d97f7355",
+            lastUpdateID: "46430",
+            title: "Component",
+            sections: [
+              {
+                __typename: "Section",
+                id: "633aebff203363a7d97f7356",
+                type: "NONLEARNING",
+                locked: true,
+                components: [
+                  {
+                    id: "633aebff203363a7d97f7357",
+                    componentName: "Text",
+                    props: '{"text":"default text"}',
+                    locked: true,
+                  },
+                ],
+              },
+              {
+                __typename: "Section",
+                id: "633aebff203363a7d97f7358",
+                type: "LEARNING",
+                locked: false,
+                components: [
+                  {
+                    componentName: "Table",
+                    props: '{}',
+                    locked: false,
+                  },
+                ],
+              },
+            ],
+            parentCourseID: "633aebff203363a7d97f7352",
+            createdAt: "1664805887360",
+            updatedAt: "1664811470175",
+          },
+          path: [
+            {
+              id: "633aebff203363a7d97f7353",
+              type: "unit",
+              name: "AMP Components",
+              indexInParent: 0,
+            },
+          ],
+          index: 1,
+        },
+  ];
+
+  if (
+    // If the returns is an array of LessonStructureContainers (getLessons)
+    Array.isArray(data) &&
+    data[0]?.__typename === "LessonStructureContainer"
+  ) {
+    convertData.dataConversionFunction(data);
+  } else {
+    // TODO: may need to check for type if __typename does not exist eg. getLesson
+  }
+  return convertData.lessons;
+};
