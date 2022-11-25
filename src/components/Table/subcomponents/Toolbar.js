3aaba48825b3f910e1d97daf41ece5f9aa219845
Mike Tkachuk <tkachuk2314@gmail.com>
2022-11-25 11:32:58 -0500

    Toolbar done-ish

--- src/components/Table/subcomponents/Toolbar.js
+++ src/components/Table/subcomponents/Toolbar.js
@@ -4,7 +4,6 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import styled from "@emotion/styled";
 
 import {
   Paper,
@@ -13,104 +12,11 @@ import {
   Grow,
   AppBar,
   Toolbar,
-  MenuItem,
   MenuList,
-  IconButton,
   Tooltip,
 } from "@mui/material";
 
 import "../../Text/styles/Toolbar.scss";
-
-// // ? Styled Container
-// const Container = styled("div")(({}) => ({
-//   position: "fixed !important",
-//   top: "100px !important",
-//   // left: "36% !important",
-//   left: "50%",
-//   transform: "translateX(-50%) !important",
-//   zIndex: 1000,
-//   gap: "10px",
-//   "& .MuiPaper-root": {
-//     backgroundColor: "transparent",
-//   },
-//   "& .StyledCard .MuiPaper-root": {
-//     backgroundColor: "#fff",
-//   },
-// }));
-
-// const StyledAppbar = styled(AppBar)({
-//   display: "flex",
-//   flexDirection: "row",
-//   minHeight: "40px !important",
-//   gap: "10px",
-//   boxShadow: "none !important",
-//   "& .MuiPaper-root": {
-//     backgroundColor: "#fff",
-//   },
-// });
-
-// const StyledFormatButton = styled(Button)({
-//   display: "flex",
-//   flexDirection: "row",
-//   backgroundColor: "#FFF",
-//   color: "#232323",
-//   fontFamily: `"Inter", sans-serif`,
-//   fontSize: "1rem",
-//   fontWeight: "400",
-//   lineHeight: "1.5rem",
-//   letterSpacing: "0.009375rem",
-//   width: "78px",
-//   padding: "8px 10px",
-//   whiteSpace: "nowrap",
-//   textAlign: "center",
-//   textTransform: "none",
-//   boxShadow: "none",
-//   "&:hover": {
-//     background: "transparent",
-//     boxShadow: "none",
-//     color: "#1565C0",
-//   },
-//   "&:active": {
-//     background: "transparent",
-//     boxShadow: "none",
-//     color: "#1565C0",
-//   },
-//   "&:focus-visible": {
-//     background: "transparent",
-//     boxShadow: "none",
-//     color: "#1565C0",
-//   },
-//   "&:disabled": { background: "none" },
-// });
-
-// const StyledTableToolbar = styled(Toolbar)({
-//   borderLeft: "4px solid #1565C0",
-//   display: "flex",
-//   justifyContent: "space-evenly",
-//   minHeight: "40px !important",
-//   margin: "10px, 7px",
-//   backgroundColor: "#FFF",
-//   boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-//   borderRadius: "4px",
-//   paddingLeft: "0px",
-//   paddingRight: "0px",
-//   paddingLeft: "0px!important",
-//   paddingRight: "0px!important",
-//   "& .MuiToolbar-gutters": {
-//     paddingLeft: "0px",
-//     paddingRight: "0px",
-//   },
-// });
-
-// const StyledFormControlLabel = styled(FormControlLabel)({
-//   height: "24px",
-//   whiteSpace: "nowrap",
-//   fontFamily: `"Inter", sans-serif`,
-//   fontSize: "1rem",
-//   fontWeight: "400",
-//   lineHeight: "1.5rem",
-//   letterSpacing: "0.009375rem",
-// });
 
 const ToolBar = ({
   toolbar,
@@ -127,166 +33,6 @@ const ToolBar = ({
   const FormatRef = useRef(null);
 
   return (
-    // <Container
-    //   onClick={(e) => e.stopPropagation()}
-    //   onFocus={(e) => e.stopPropagation()}
-    //   className="ToolbarContainer"
-    //   style={{
-    //     "--active": toolbar ? "block" : "none",
-    //   }}
-    // >
-    //   <StyledAppbar position="static">
-    //     <StyledTableToolbar
-    //       position="static"
-    //       className="StyledToolbar"
-    //       style={{
-    //         "--width": "8.4375rem",
-    //       }}
-    //     >
-    //       <Button className="SelectButton">
-    //         <Tooltip aria-label="Format" title="Format" placement="top" arrow>
-    //           <StyledFormatButton onClick={(e) => {setShowFormat(!showFormat)}}
-    //           >Format
-    //           </StyledFormatButton>
-    //         </Tooltip>
-    //       </Button>
-    //       <Paper style={{
-    //         "display": showFormat ? "block" :"none",
-    //         "padding": "23px 26px",
-    //         "width": "220px",
-    //         "position": "absolute",
-    //         "top": "41px",
-    //         "left": "0px",
-    //         "boxShadow": "0px 5px 5px -3px rgb(0 0 0 / 20%), 0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%)",
-    //         "borderRadius": "4px",
-    //         }}>
-    //         <FormGroup sx={{ gap: "14px" }}>
-    //           {headerType == "top-header" && (
-    //             <FormControl>
-    //               <Tooltip
-    //                 aria-label="Show top headers"
-    //                 title="Show top headers"
-    //                 placement="top"
-    //                 arrow
-    //                 PopperProps={{
-    //                   modifiers: [
-    //                     {
-    //                       name: "offset",
-    //                       options: {
-    //                         offset: [0, -7],
-    //                       },
-    //                     },
-    //                   ],
-    //                 }}
-    //               >
-    //                 <StyledFormControlLabel
-    //                   control={
-    //                     <Checkbox
-    //                       onChange={() => setShowTopHeader(!showTopHeader)}
-    //                       checked={!showTopHeader}
-    //                       sx={{
-    //                         "&:hover": {
-    //                           bgcolor: "transparent",
-    //                           color: "rgba(21, 101, 192, 1)",
-    //                         },
-    //                         "&.Mui-checked": {
-    //                           bgcolor: "transparent",
-    //                           color: "rgba(21, 101, 192, 1)",
-    //                         },
-    //                       }}
-    //                     />
-    //                   }
-    //                   label="Show top headers"
-    //                   size="small"
-    //                 />
-    //               </Tooltip>
-    //             </FormControl>
-    //           )}
-    //           {headerType == "side-header" && (
-    //             <FormControl>
-    //               <Tooltip
-    //                 aria-label="Show side headers"
-    //                 title="Show side headers"
-    //                 placement="top"
-    //                 arrow
-    //                 PopperProps={{
-    //                   modifiers: [
-    //                     {
-    //                       name: "offset",
-    //                       options: {
-    //                         offset: [0, -7],
-    //                       },
-    //                     },
-    //                   ],
-    //                 }}
-    //               >
-    //                 <StyledFormControlLabel
-    //                   control={
-    //                     <Checkbox
-    //                       onChange={() => setShowSideHeader(!showSideHeader)}
-    //                       checked={!showSideHeader}
-    //                       sx={{
-    //                         "&:hover": {
-    //                           bgcolor: "transparent",
-    //                           color: "rgba(21, 101, 192, 1)",
-    //                         },
-    //                         "&.Mui-checked": {
-    //                           bgcolor: "transparent",
-    //                           color: "rgba(21, 101, 192, 1)",
-    //                         },
-    //                       }}
-    //                     />
-    //                   }
-    //                   label="Show side headers"
-    //                   size="small"
-    //                 />
-    //               </Tooltip>
-    //             </FormControl>
-    //           )}
-
-    //           <FormControl>
-    //             <Tooltip
-    //               aria-label="Show zebra stripes"
-    //               title="Show zebra stripes"
-    //               placement="top"
-    //               arrow
-    //               PopperProps={{
-    //                 modifiers: [
-    //                   {
-    //                     name: "offset",
-    //                     options: {
-    //                       offset: [0, -7],
-    //                     },
-    //                   },
-    //                 ],
-    //               }}
-    //             >
-    //               <StyledFormControlLabel
-    //                 control={
-    //                   <Checkbox
-    //                     onChange={() => setZebraStripes(!zebraStripes)}
-    //                     sx={{
-    //                       "&:hover": {
-    //                         bgcolor: "transparent",
-    //                         color: "rgba(21, 101, 192, 1)",
-    //                       },
-    //                       "&.Mui-checked": {
-    //                         bgcolor: "transparent",
-    //                         color: "rgba(21, 101, 192, 1)",
-    //                       },
-    //                     }}
-    //                   />
-    //                 }
-    //                 label="Show zebra stripes"
-    //                 size="small"
-    //               />
-    //             </Tooltip>
-    //           </FormControl>
-    //         </FormGroup>
-    //       </Paper>
-    //     </StyledTableToolbar>
-    //   </StyledAppbar>
-    // </Container>
     <div
       onClick={(e) => e.stopPropagation()}
       onFocus={(e) => e.stopPropagation()}
@@ -359,6 +105,14 @@ const ToolBar = ({
             placement="bottom-start"
             transition
             disablePortal
+            modifiers={[
+              {
+                name: "offset",
+                options: {
+                  offset: [-5, -5],
+                },
+              },
+            ]}
           >
             {({ TransitionProps }) => (
               <Grow {...TransitionProps}>
