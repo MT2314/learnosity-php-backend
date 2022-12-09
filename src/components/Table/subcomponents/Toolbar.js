cbbe69f9af85377f01cc230fbaed8fc77c779152
Mike Tkachuk <tkachuk2314@gmail.com>
2022-12-08 23:12:29 -0500

    Close format on clickaway

--- src/components/Table/subcomponents/Toolbar.js
+++ src/components/Table/subcomponents/Toolbar.js
@@ -14,13 +14,11 @@ import {
   Toolbar,
   MenuItem,
   ClickAwayListener,
-  Divider,
   Button,
   IconButton,
   Popper,
   Grow,
   Tooltip,
-  Card,
   MenuList,
 } from "@mui/material";
 
@@ -234,7 +232,6 @@ const ToolBar = ({
         selectSection == null,
     },
   ];
-  console.log(selectSection);
   return (
     <div
       onClick={(e) => e.stopPropagation()}
@@ -380,7 +377,6 @@ const ToolBar = ({
               color="inherit"
               onClick={() => {
                 closeDropDown();
-                console.log("Move column right");
               }}
               // If needed to add onKeyDown
               onKeyDown={(e) => {}}
@@ -415,7 +411,6 @@ const ToolBar = ({
               color="inherit"
               onClick={() => {
                 closeDropDown();
-                console.log("Move row up");
               }}
               // If needed to add onKeyDown
               onKeyDown={(e) => {}}
@@ -445,7 +440,6 @@ const ToolBar = ({
               color="inherit"
               onClick={() => {
                 closeDropDown();
-                console.log("Move row down");
               }}
               // If needed to add onKeyDown
               onKeyDown={(e) => {}}
@@ -513,166 +507,168 @@ const ToolBar = ({
             {({ TransitionProps }) => (
               <Grow {...TransitionProps}>
                 <Paper>
-                  <MenuList
-                    data-testid="format-settings-dropdown"
-                    aria-labelledby="Format Settings"
-                    className="StyledCheckboxMenu"
-                    style={{
-                      "--width": "220px",
-                      "--height": "auto",
-                      "--padding": "8px 0",
-                    }}
-                  >
-                    <FormGroup
-                      sx={{ gap: "14px", margin: "8px 16px !important" }}
+                  <ClickAwayListener onClickAway={() => setShowFormat(false)}>
+                    <MenuList
+                      data-testid="format-settings-dropdown"
+                      aria-labelledby="Format Settings"
+                      className="StyledCheckboxMenu"
+                      style={{
+                        "--width": "220px",
+                        "--height": "auto",
+                        "--padding": "8px 0",
+                      }}
                     >
-                      {/* Top Header */}
-                      {state.headerType == "top-header" && (
-                        <FormControl>
-                          <Tooltip
-                            aria-label="show top header"
-                            title="show top header"
-                            placement="top"
-                            arrow
-                            PopperProps={{
-                              modifiers: [
-                                {
-                                  name: "offset",
-                                  options: {
-                                    offset: [0, -7],
-                                  },
-                                },
-                              ],
-                            }}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                            }}
-                          >
-                            <FormControlLabel
-                              className="StyledFormControlLabel"
-                              control={
-                                <Checkbox
-                                  checked={!state.hideTopHeader}
-                                  onClick={(e) => {
-                                    e.stopPropagation();
-                                    hideTopHeader();
-                                  }}
-                                  sx={{
-                                    "&:hover": {
-                                      bgcolor: "transparent",
-                                      color: "rgba(21, 101, 192, 1)",
+                      <FormGroup
+                        sx={{ gap: "14px", margin: "8px 16px !important" }}
+                      >
+                        {/* Top Header */}
+                        {state.headerType == "top-header" && (
+                          <FormControl>
+                            <Tooltip
+                              aria-label="show top header"
+                              title="show top header"
+                              placement="top"
+                              arrow
+                              PopperProps={{
+                                modifiers: [
+                                  {
+                                    name: "offset",
+                                    options: {
+                                      offset: [0, -7],
                                     },
-                                    "&.Mui-checked": {
-                                      bgcolor: "transparent",
-                                      color: "rgba(21, 101, 192, 1)",
-                                    },
-                                  }}
-                                />
-                              }
-                              label="Show top header"
-                              size="small"
-                            />
-                          </Tooltip>
-                        </FormControl>
-                      )}
-                      {/* Side Header */}
-                      {state.headerType == "side-header" && (
-                        <FormControl>
-                          <Tooltip
-                            aria-label="show side header"
-                            title="show side header"
-                            placement="top"
-                            arrow
-                            PopperProps={{
-                              modifiers: [
-                                {
-                                  name: "offset",
-                                  options: {
-                                    offset: [0, -7],
                                   },
-                                },
-                              ],
-                            }}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                            }}
-                          >
-                            <FormControlLabel
-                              className="StyledFormControlLabel"
-                              control={
-                                <Checkbox
-                                  checked={!state.hideSideHeader}
-                                  onClick={(e) => {
-                                    e.stopPropagation();
-                                    hideSideHeader();
-                                  }}
-                                  sx={{
-                                    "&:hover": {
-                                      bgcolor: "transparent",
-                                      color: "rgba(21, 101, 192, 1)",
-                                    },
-                                    "&.Mui-checked": {
-                                      bgcolor: "transparent",
-                                      color: "rgba(21, 101, 192, 1)",
-                                    },
-                                  }}
-                                />
-                              }
-                              label="Show side header"
-                              size="small"
-                            />
-                          </Tooltip>
-                        </FormControl>
-                      )}
-                      {/* Zebra Stripes */}
-                      <FormControl>
-                        <Tooltip
-                          aria-label="show zebra stripes"
-                          title="show zebra stripes"
-                          placement="top"
-                          arrow
-                          PopperProps={{
-                            modifiers: [
-                              {
-                                name: "offset",
-                                options: {
-                                  offset: [0, -7],
-                                },
-                              },
-                            ],
-                          }}
-                          onClick={(e) => {
-                            e.stopPropagation();
-                          }}
-                        >
-                          <FormControlLabel
-                            className="StyledFormControlLabel"
-                            control={
-                              <Checkbox
-                                checked={state.showStripes}
-                                onClick={(e) => {
-                                  e.stopPropagation();
-                                  showZebraStripes();
-                                }}
-                                sx={{
-                                  "&:hover": {
-                                    bgcolor: "transparent",
-                                    color: "rgba(21, 101, 192, 1)",
-                                  },
-                                  "&.Mui-checked": {
-                                    bgcolor: "transparent",
-                                    color: "rgba(21, 101, 192, 1)",
-                                  },
-                                }}
+                                ],
+                              }}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                              }}
+                            >
+                              <FormControlLabel
+                                className="StyledFormControlLabel"
+                                control={
+                                  <Checkbox
+                                    checked={!state.hideTopHeader}
+                                    onClick={(e) => {
+                                      e.stopPropagation();
+                                      hideTopHeader();
+                                    }}
+                                    sx={{
+                                      "&:hover": {
+                                        bgcolor: "transparent",
+                                        color: "rgba(21, 101, 192, 1)",
+                                      },
+                                      "&.Mui-checked": {
+                                        bgcolor: "transparent",
+                                        color: "rgba(21, 101, 192, 1)",
+                                      },
+                                    }}
+                                  />
+                                }
+                                label="Show top header"
+                                size="small"
                               />
-                            }
-                            label="Show zebra stripes"
-                            size="small"
-                          />
-                        </Tooltip>
-                      </FormControl>
-                    </FormGroup>
-                  </MenuList>
+                            </Tooltip>
+                          </FormControl>
+                        )}
+                        {/* Side Header */}
+                        {state.headerType == "side-header" && (
+                          <FormControl>
+                            <Tooltip
+                              aria-label="show side header"
+                              title="show side header"
+                              placement="top"
+                              arrow
+                              PopperProps={{
+                                modifiers: [
+                                  {
+                                    name: "offset",
+                                    options: {
+                                      offset: [0, -7],
+                                    },
+                                  },
+                                ],
+                              }}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                              }}
+                            >
+                              <FormControlLabel
+                                className="StyledFormControlLabel"
+                                control={
+                                  <Checkbox
+                                    checked={!state.hideSideHeader}
+                                    onClick={(e) => {
+                                      e.stopPropagation();
+                                      hideSideHeader();
+                                    }}
+                                    sx={{
+                                      "&:hover": {
+                                        bgcolor: "transparent",
+                                        color: "rgba(21, 101, 192, 1)",
+                                      },
+                                      "&.Mui-checked": {
+                                        bgcolor: "transparent",
+                                        color: "rgba(21, 101, 192, 1)",
+                                      },
+                                    }}
+                                  />
+                                }
+                                label="Show side header"
+                                size="small"
+                              />
+                            </Tooltip>
+                          </FormControl>
+                        )}
+                        {/* Zebra Stripes */}
+                        <FormControl>
+                          <Tooltip
+                            aria-label="show zebra stripes"
+                            title="show zebra stripes"
+                            placement="top"
+                            arrow
+                            PopperProps={{
+                              modifiers: [
+                                {
+                                  name: "offset",
+                                  options: {
+                                    offset: [0, -7],
+                                  },
+                                },
+                              ],
+                            }}
+                            onClick={(e) => {
+                              e.stopPropagation();
+                            }}
+                          >
+                            <FormControlLabel
+                              className="StyledFormControlLabel"
+                              control={
+                                <Checkbox
+                                  checked={state.showStripes}
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    showZebraStripes();
+                                  }}
+                                  sx={{
+                                    "&:hover": {
+                                      bgcolor: "transparent",
+                                      color: "rgba(21, 101, 192, 1)",
+                                    },
+                                    "&.Mui-checked": {
+                                      bgcolor: "transparent",
+                                      color: "rgba(21, 101, 192, 1)",
+                                    },
+                                  }}
+                                />
+                              }
+                              label="Show zebra stripes"
+                              size="small"
+                            />
+                          </Tooltip>
+                        </FormControl>
+                      </FormGroup>
+                    </MenuList>
+                  </ClickAwayListener>
                 </Paper>
               </Grow>
             )}
