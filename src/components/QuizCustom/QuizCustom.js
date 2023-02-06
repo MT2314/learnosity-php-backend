7c1cabbdcf550fbf3bceb458fa5d1a65818abd99
Mike Tkachuk <tkachuk2314@gmail.com>
2023-02-06 05:19:09 -0500

    Custom Get item edit and item list

--- src/components/QuizCustom/QuizCustom.js
+++ src/components/QuizCustom/QuizCustom.js
@@ -15,17 +15,18 @@ const QuizCustom = () => {
   const authorScript = useScript(authorAPI);
 
   // Request the Learnosity Author API configuration
-  const requestAPI = async () => {
+  const postCustomRequest = async (mode) => {
     try {
       const response = await axios.get(
-        `http://127.0.0.1/learnosity-demos/www/authoring/item-create.php`,
+        "http://127.0.0.1/learnosity-demos/www/authoring/item-create_customPost.php",
         {
           headers: { "Content-Type": "application/json" },
-          params: {},
+          params: {
+            mode: mode,
+          },
         }
       );
       let parsedRequest = JSON.parse(response.data.request);
-
       setResponse(parsedRequest);
       setAuthorAPI(response.data.url_authorapi);
       return response.data;
@@ -34,27 +35,46 @@ const QuizCustom = () => {
     }
   };
 
-  // Load the Author API configuration when the component mounts
-  useEffect(() => {
-    requestAPI();
-  }, []);
+  // // Load the Author API configuration when the component mounts
+  // useEffect(() => {
+  //   requestAPI();
+  // }, []);
 
   // Initialize the Author API when the script is loaded
   useEffect(() => {
+    let itemsApp;
     if (authorScript === "ready" && response !== null) {
-      LearnosityAuthor.init(response, {
-        readyListener() {},
+      itemsApp = LearnosityAuthor.init(response, {
+        readyListener() {
+          itemsApp.destroy();
+        },
         errorListener(err) {
           console.log("error", err);
         },
       });
     }
+    console.log("itemsApp", itemsApp);
   }, [response, authorScript]);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
+          <div>
+            <button
+              type="button"
+              onClick={() => postCustomRequest("item_list")}
+            >
+              Select existing quiz item
+            </button>
+            <button
+              type="button"
+              onClick={() => postCustomRequest("item_edit")}
+            >
+              Create new quiz item
+            </button>
+          </div>
+
           <h1>Standalone Assessment Example</h1>
 
           <div id="learnosity-author"></div>
